e68b881fdf04a17f32ac0ab3aad88004501c6825
RTMecha <mechaprower2022@gmail.com>
2024-05-05 22:54:58 +1000

    Added Creator helper class

---
+++ Core/Creator.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace BetterLegacy.Core
+{
+    public static class Creator
+    {
+        public static GameObject NewGameObject(string name, Transform parent)
+        {
+            var gameObject = new GameObject(name);
+            gameObject.transform.SetParent(parent);
+            gameObject.transform.localScale = Vector3.one;
+            return gameObject;
+        }
+
+        public static GameObject NewUIObject(string name, Transform parent)
+        {
+            var gameObject = NewGameObject(name, parent);
+            gameObject.AddComponent<RectTransform>();
+
+            return gameObject;
+        }
+    }
+}
