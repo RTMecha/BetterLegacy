99e2eb525b028170a7a59f60817761c472df8413
RTMecha <mechaprower2022@gmail.com>
2024-05-06 22:19:52 +1000

    Clean up code a bit

--- Components/SelectGUI.cs
+++ Components/SelectGUI.cs
@@ -1,11 +1,12 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 using UnityEngine.EventSystems;
 
 namespace BetterLegacy.Components
 {
     public class SelectGUI : MonoBehaviour, IEventSystemHandler, IPointerDownHandler, IPointerUpHandler
     {
-        bool dragging;
+        public bool dragging;
         Vector3 startMousePos;
         Vector3 startPos;
 
@@ -15,58 +16,63 @@ namespace BetterLegacy.Components
 
         public static bool DragGUI { get; set; }
         public bool OverrideDrag { get; set; }
+        public bool CanDrag => DragGUI || OverrideDrag;
+
+        public Action<Vector2> draggingAction;
 
         public void OnPointerUp(PointerEventData eventData)
         {
-            if (DragGUI || OverrideDrag)
-            {
-                AudioManager.inst.PlaySound("blip");
-                target.localScale = new Vector3(1f, 1f, 1f);
-                dragging = false;
-            }
+            if (!CanDrag)
+                return;
+
+            AudioManager.inst.PlaySound("blip");
+            target.localScale = new Vector3(1f, 1f, 1f);
+            dragging = false;
         }
 
         public void OnPointerDown(PointerEventData eventData)
         {
-            if (DragGUI || OverrideDrag)
+            if (!CanDrag)
+                return;
+
+            if (!Input.GetMouseButtonDown(2))
             {
-                if (!Input.GetMouseButtonDown(2))
-                {
-                    AudioManager.inst.PlaySound("Click");
-                    target.localScale = new Vector3(scale, scale, 1f);
-                    dragging = true;
-                    startMousePos = new Vector3(Input.mousePosition.x, Input.mousePosition.y, 0f);
-                    startPos = target.position;
-                }
-                else
-                {
-                    AudioManager.inst.PlaySound("Click");
-                    target.position = ogPos;
-                    startMousePos = new Vector3(Input.mousePosition.x, Input.mousePosition.y, 0f);
-                    startPos = target.position;
-                }
+                AudioManager.inst.PlaySound("Click");
+                target.localScale = new Vector3(scale, scale, 1f);
+                dragging = true;
+                startMousePos = new Vector3(Input.mousePosition.x, Input.mousePosition.y, 0f);
+                startPos = target.position;
+            }
+            else
+            {
+                AudioManager.inst.PlaySound("Click");
+                target.position = ogPos;
+                startMousePos = new Vector3(Input.mousePosition.x, Input.mousePosition.y, 0f);
+                startPos = target.position;
             }
         }
 
         void Update()
         {
-            if (dragging)
+            if (!dragging)
+                return;
+
+            var vector = new Vector3(Input.mousePosition.x, Input.mousePosition.y, target.localPosition.z);
+            if (Input.GetKey(KeyCode.LeftShift))
             {
-                Vector3 vector = new Vector3(Input.mousePosition.x, Input.mousePosition.y, target.localPosition.z);
-                if (Input.GetKey(KeyCode.LeftShift))
-                {
-                    if (Mathf.Abs(startMousePos.x - vector.x) > Mathf.Abs(startMousePos.y - vector.y))
-                        target.position = new Vector3(vector.x, target.position.y);
-                    if (Mathf.Abs(startMousePos.x - vector.x) < Mathf.Abs(startMousePos.y - vector.y))
-                        target.position = new Vector3(target.position.x, vector.y);
-                }
-                else
-                {
-                    float x = startMousePos.x - vector.x;
-                    float y = startMousePos.y - vector.y;
-                    target.position = new Vector3(startPos.x + -x, startPos.y + -y);
-                }
+                if (Mathf.Abs(startMousePos.x - vector.x) > Mathf.Abs(startMousePos.y - vector.y))
+                    target.position = new Vector3(vector.x, target.position.y);
+                if (Mathf.Abs(startMousePos.x - vector.x) < Mathf.Abs(startMousePos.y - vector.y))
+                    target.position = new Vector3(target.position.x, vector.y);
             }
+            else
+            {
+                float x = startMousePos.x - vector.x;
+                float y = startMousePos.y - vector.y;
+                target.position = new Vector3(startPos.x + -x, startPos.y + -y);
+            }
+
+            draggingAction?.Invoke(target.position);
         }
     }
 }
