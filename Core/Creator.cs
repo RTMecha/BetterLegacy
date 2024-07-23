1cc62c0ea2c9ed51d710d97d7356485cc6ea3a56
RTMecha <mechaprower2022@gmail.com>
2024-07-24 04:07:40 +1000

    Added siblingIndex param

--- Core/Creator.cs
+++ Core/Creator.cs
@@ -8,17 +8,20 @@ namespace BetterLegacy.Core
 {
     public static class Creator
     {
-        public static GameObject NewGameObject(string name, Transform parent)
+        public static GameObject NewGameObject(string name, Transform parent, int siblingIndex = -1)
         {
             var gameObject = new GameObject(name);
             gameObject.transform.SetParent(parent);
+            if (siblingIndex != -1)
+                gameObject.transform.SetSiblingIndex(siblingIndex);
             gameObject.transform.localScale = Vector3.one;
+
             return gameObject;
         }
 
-        public static GameObject NewUIObject(string name, Transform parent)
+        public static GameObject NewUIObject(string name, Transform parent, int siblingIndex = -1)
         {
-            var gameObject = NewGameObject(name, parent);
+            var gameObject = NewGameObject(name, parent, siblingIndex);
             gameObject.AddComponent<RectTransform>();
 
             return gameObject;
