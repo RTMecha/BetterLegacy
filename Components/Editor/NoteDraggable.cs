bc87df3f053a6d6defcac6c55d144871c3422e8d
RTMecha <mechaprower2022@gmail.com>
2024-08-10 22:28:12 +1000

    Fixed resolution scaling

--- Components/Editor/NoteDraggable.cs
+++ Components/Editor/NoteDraggable.cs
@@ -1,4 +1,5 @@
-﻿using BetterLegacy.Editor.Managers;
+﻿using BetterLegacy.Core.Helpers;
+using BetterLegacy.Editor.Managers;
 using System;
 using UnityEngine;
 using UnityEngine.EventSystems;
@@ -20,31 +21,31 @@ namespace BetterLegacy.Components.Editor
 
         public ProjectPlannerManager.NoteItem note;
 
-        public Action onClick;
-
         bool dragging;
         Vector3 startMousePos;
         Vector3 startPos;
         Vector3 startSca;
 
-        public float scale = 0.03f;
-
-        public bool Free => ProjectPlannerManager.inst && (!ProjectPlannerManager.inst.PlannerActive || ProjectPlannerManager.inst.CurrentTab != 5);
+        /// <summary>
+        /// Gets a true value if the note is in floating mode, otherwise gets a false value if it's in the Project Planner layout.
+        /// </summary>
+        public static bool Free => ProjectPlannerManager.inst && (!ProjectPlannerManager.inst.PlannerActive || ProjectPlannerManager.inst.CurrentTab != 5);
 
         public void OnPointerUp(PointerEventData eventData)
         {
-            if (Free)
+            if (!Free)
+                return;
+
+            AudioManager.inst.PlaySound("blip");
+            if (part == DragPart.Base)
             {
-                AudioManager.inst.PlaySound("blip");
-                if (part == DragPart.Base)
-                {
-                    transform.localScale = new Vector3(note.Scale.x, note.Scale.y, 1f);
-                    note.Position = transform.localPosition;
-                    note.Dragging = false;
-                }
-                ProjectPlannerManager.inst?.SaveNotes();
-                dragging = false;
+                transform.localScale = new Vector3(note.Scale.x, note.Scale.y, 1f);
+                note.Position = transform.localPosition;
+                note.Dragging = false;
             }
+
+            ProjectPlannerManager.inst?.SaveNotes();
+            dragging = false;
         }
 
         public void OnPointerDown(PointerEventData eventData)
@@ -53,135 +54,127 @@ namespace BetterLegacy.Components.Editor
             {
                 AudioManager.inst.PlaySound("Click");
                 dragging = true;
-                if (part == DragPart.Base)
-                {
-                    transform.localScale = new Vector3(note.Scale.x + scale, note.Scale.y + scale, 1f);
-                    note.Dragging = true;
-                }
-                startMousePos = new Vector3(Input.mousePosition.x, Input.mousePosition.y, 0f);
 
                 if (part == DragPart.Base)
                 {
+                    transform.localScale = new Vector3(note.Scale.x + 0.03f, note.Scale.y + 0.03f, 1f);
+                    note.Dragging = true;
+                    startMousePos = new Vector3(Input.mousePosition.x, Input.mousePosition.y, 0f);
                     startPos = transform.position;
                 }
                 else
                 {
+                    startMousePos = new Vector3(Input.mousePosition.x, Input.mousePosition.y, 0f) * CoreHelper.ScreenScaleInverse;
                     startPos = note.Size;
                     startSca = note.Scale;
                 }
             }
 
             if (!Free && part == DragPart.Base)
-            {
-                onClick?.Invoke();
-            }
+                ProjectPlannerManager.inst?.OpenNoteEditor(note);
         }
 
         void Update()
         {
-            if (dragging)
+            if (!dragging)
+                return;
+
+            var vector = new Vector3(Input.mousePosition.x, Input.mousePosition.y, 0f) * (part == DragPart.Base ? 1f : CoreHelper.ScreenScaleInverse);
+
+            switch (part)
             {
-                var vector = new Vector3(Input.mousePosition.x, Input.mousePosition.y, transform.localPosition.z);
-
-                switch (part)
-                {
-                    case DragPart.Base:
+                case DragPart.Base:
+                    {
+                        if (Input.GetKey(KeyCode.LeftShift))
                         {
-                            if (Input.GetKey(KeyCode.LeftShift))
-                            {
-                                transform.position = new Vector3((int)vector.x, (int)vector.y);
-                            }
-                            else
-                            {
-                                float x = startMousePos.x - vector.x;
-                                float y = startMousePos.y - vector.y;
-                                transform.position = new Vector3(startPos.x + -x, startPos.y + -y);
-                            }
-                            break;
+                            transform.position = new Vector3((int)vector.x, (int)vector.y);
                         }
-                    case DragPart.Left:
+                        else
                         {
-                            if (Input.GetKey(KeyCode.LeftControl))
-                            {
-                                var sca = note.Scale;
-                                sca.x = Mathf.Clamp(startSca.x + -(startMousePos.x - vector.x) * 0.005f, 0.2f, 2f);
-                                note.Scale = sca;
-                            }
-                            else
-                            {
-                                var size = note.Size;
-                                size.x = Mathf.Clamp(startPos.x + -(startMousePos.x - vector.x) * 2f, 132f, 1700f);
-                                note.Size = size;
-                            }
-
-                            break;
+                            float x = startMousePos.x - vector.x;
+                            float y = startMousePos.y - vector.y;
+                            transform.position = new Vector3(startPos.x + -x, startPos.y + -y);
                         }
-                    case DragPart.Right:
+                        break;
+                    }
+                case DragPart.Left:
+                    {
+                        if (Input.GetKey(KeyCode.LeftControl))
                         {
-                            if (Input.GetKey(KeyCode.LeftControl))
-                            {
-                                var sca = note.Scale;
-                                sca.x = Mathf.Clamp(startSca.x + (startMousePos.x - vector.x) * 0.005f, 0.2f, 2f);
-                                note.Scale = sca;
-                            }
-                            else
-                            {
-                                var size = note.Size;
-                                size.x = Mathf.Clamp(startPos.x + (startMousePos.x - vector.x) * 2f, 132f, 1700f);
-                                note.Size = size;
-                            }
-
-                            break;
+                            var sca = note.Scale;
+                            sca.x = Mathf.Clamp(startSca.x + -(startMousePos.x - vector.x) * 0.005f, 0.2f, 2f);
+                            note.Scale = sca;
                         }
-                    case DragPart.Up:
+                        else
                         {
-                            if (Input.GetKey(KeyCode.LeftControl))
-                            {
-                                var sca = note.Scale;
-                                sca.y = Mathf.Clamp(startSca.y + -(startMousePos.y - vector.y) * 0.005f, 0.2f, 2f);
-                                note.Scale = sca;
-                            }
-                            else
-                            {
-                                var size = note.Size;
-                                size.y = Mathf.Clamp(startPos.y + -(startMousePos.y - vector.y) * 2f, 32f, 1000f);
-                                note.Size = size;
-                            }
-
-                            break;
+                            var size = note.Size;
+                            size.x = Mathf.Clamp(startPos.x + -(startMousePos.x - vector.x) * 2f, 132f, 1700f);
+                            note.Size = size;
                         }
-                    case DragPart.Down:
+
+                        break;
+                    }
+                case DragPart.Right:
+                    {
+                        if (Input.GetKey(KeyCode.LeftControl))
                         {
-                            if (Input.GetKey(KeyCode.LeftControl))
-                            {
-                                var sca = note.Scale;
-                                sca.y = Mathf.Clamp(startSca.y + (startMousePos.y - vector.y) * 0.005f, 0.2f, 2f);
-                                note.Scale = sca;
-                            }
-                            else
-                            {
-                                var size = note.Size;
-                                size.y = Mathf.Clamp(startPos.y + (startMousePos.y - vector.y) * 2f, 32f, 1000f);
-                                note.Size = size;
-                            }
-
-                            break;
+                            var sca = note.Scale;
+                            sca.x = Mathf.Clamp(startSca.x + (startMousePos.x - vector.x) * 0.005f, 0.2f, 2f);
+                            note.Scale = sca;
                         }
-                }
+                        else
+                        {
+                            var size = note.Size;
+                            size.x = Mathf.Clamp(startPos.x + (startMousePos.x - vector.x) * 2f, 132f, 1700f);
+                            note.Size = size;
+                        }
+
+                        break;
+                    }
+                case DragPart.Up:
+                    {
+                        if (Input.GetKey(KeyCode.LeftControl))
+                        {
+                            var sca = note.Scale;
+                            sca.y = Mathf.Clamp(startSca.y + -(startMousePos.y - vector.y) * 0.005f, 0.2f, 2f);
+                            note.Scale = sca;
+                        }
+                        else
+                        {
+                            var size = note.Size;
+                            size.y = Mathf.Clamp(startPos.y + -(startMousePos.y - vector.y) * 2f, 32f, 1000f);
+                            note.Size = size;
+                        }
+
+                        break;
+                    }
+                case DragPart.Down:
+                    {
+                        if (Input.GetKey(KeyCode.LeftControl))
+                        {
+                            var sca = note.Scale;
+                            sca.y = Mathf.Clamp(startSca.y + (startMousePos.y - vector.y) * 0.005f, 0.2f, 2f);
+                            note.Scale = sca;
+                        }
+                        else
+                        {
+                            var size = note.Size;
+                            size.y = Mathf.Clamp(startPos.y + (startMousePos.y - vector.y) * 2f, 32f, 1000f);
+                            note.Size = size;
+                        }
+
+                        break;
+                    }
             }
         }
 
         public void OnPointerEnter(PointerEventData eventData)
         {
             if (Free && (part == DragPart.Left || part == DragPart.Right))
-            {
                 Cursor.SetCursor(ProjectPlannerManager.inst.horizontalDrag, CursorMode.Auto);
-            }
 
             if (Free && (part == DragPart.Up || part == DragPart.Down))
-            {
                 Cursor.SetCursor(ProjectPlannerManager.inst.verticalDrag, CursorMode.Auto);
-            }
         }
 
         public void OnPointerExit(PointerEventData eventData)
