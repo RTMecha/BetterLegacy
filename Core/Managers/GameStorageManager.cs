eb3e0a4345c454cff5ece05181d337123a3ee9d7
RTMecha <mechaprower2022@gmail.com>
2024-08-18 14:07:53 +1000

    Removed custom blur bg since it didn't quite work the way I wanted it to

--- Core/Managers/GameStorageManager.cs
+++ Core/Managers/GameStorageManager.cs
@@ -1,6 +1,7 @@
 ﻿using BetterLegacy.Components.Editor;
 using BetterLegacy.Configs;
 using BetterLegacy.Core.Data;
+using BetterLegacy.Core.Helpers;
 using BetterLegacy.Editor;
 using BetterLegacy.Editor.Managers;
 using System;
@@ -20,33 +21,21 @@ namespace BetterLegacy.Core.Managers
         void Awake()
         {
             inst = this;
-            perspectiveCam = GameManager.inst.CameraPerspective.GetComponent<Camera>();
             postProcessLayer = Camera.main.gameObject.GetComponent<PostProcessLayer>();
             extraBG = GameObject.Find("ExtraBG").transform;
             video = extraBG.GetChild(0);
-
-            try
-            {
-                bgMaterial = BackgroundManager.inst.backgroundPrefab.GetComponent<MeshRenderer>().material;
-                interfaceBlur = GameManager.inst.menuUI.GetComponentInChildren<Image>();
-                playerGUICanvasScaler = GameManager.inst.playerGUI.GetComponent<CanvasScaler>();
-            }
-            catch (Exception ex)
-            {
-                Debug.LogException(ex);
-            }
 
             timelinePlayer = GameManager.inst.timeline.transform.Find("Base/position").GetComponent<Image>();
             timelineLeftCap = GameManager.inst.timeline.transform.Find("Base/Image").GetComponent<Image>();
             timelineRightCap = GameManager.inst.timeline.transform.Find("Base/Image 1").GetComponent<Image>();
             timelineLine = GameManager.inst.timeline.transform.Find("Base").GetComponent<Image>();
 
-            var objectDragger = new GameObject("Dragger");
-            objectDragger.transform.SetParent(GameManager.inst.transform.parent);
-            objectDragger.transform.localScale = Vector3.one;
-            this.objectDragger = objectDragger.transform;
+            if (!CoreHelper.InEditor)
+                return;
 
-            var rotator = ObjectManager.inst.objectPrefabs[1].options[4].transform.GetChild(0).gameObject.Duplicate(this.objectDragger, "Rotator");
+            objectDragger = Creator.NewGameObject("Dragger", GameManager.inst.transform.parent).transform;
+
+            var rotator = ObjectManager.inst.objectPrefabs[1].options[4].transform.GetChild(0).gameObject.Duplicate(objectDragger, "Rotator");
             Destroy(rotator.GetComponent<SelectObjectInEditor>());
             rotator.tag = "Helper";
             rotator.transform.localScale = new Vector3(2f, 2f, 1f);
@@ -58,48 +47,23 @@ namespace BetterLegacy.Core.Managers
 
             rotator.SetActive(true);
 
-            objectDragger.SetActive(false);
+            objectDragger.gameObject.SetActive(false);
 
             objectScalerTop = CreateScaler(Axis.PosY, Color.green);
             objectScalerLeft = CreateScaler(Axis.PosX, Color.red);
             objectScalerBottom = CreateScaler(Axis.NegY, Color.green);
             objectScalerRight = CreateScaler(Axis.NegX, Color.red);
-
-            if (!EditorManager.inst)
-            {
-                playerGUIBack = GameManager.inst.menuUI.GetComponentInChildren<Image>();
-                playerGUIBack.gameObject.SetActive(false);
-
-                var guiBlurObject = ObjectManager.inst.objectPrefabs[0].options[0].transform.GetChild(0).gameObject.Duplicate(transform.parent, "blur");
-                guiBlur = guiBlurObject.GetComponent<Renderer>();
-                guiBlur.enabled = true;
-                guiBlurObject.transform.localScale = new Vector3(1000f, 1000f, 1f);
-
-                guiBlur.material = ObjectManager.inst.norm;
-                guiBlur.material.shader = LegacyPlugin.blurColored;
-                guiBlur.material.SetFloat("_Size", CoreConfig.Instance.InterfaceBlurSize.Value);
-                guiBlur.material.color = CoreConfig.Instance.InterfaceBlurColor.Value;
-
-                Destroy(guiBlurObject.GetComponent<SelectObjectInEditor>());
-                Destroy(guiBlurObject.GetComponent<Collider2D>());
-            }
         }
 
         void Update()
         {
-            objectDragger.gameObject.SetActive(EditorManager.inst && EditorManager.inst.isEditing &&
+            if (!CoreHelper.InEditor)
+                return;
+
+            objectDragger.gameObject.SetActive(CoreHelper.InEditor && EditorManager.inst.isEditing &&
                 ObjectEditor.inst.SelectedObjectCount == 1 &&
                 (ObjectEditor.inst.CurrentSelection.IsBeatmapObject && ObjectEditor.inst.CurrentSelection.GetData<BeatmapObject>().objectType != BeatmapObject.ObjectType.Empty || ObjectEditor.inst.CurrentSelection.IsPrefabObject) &&
                 RTObject.Enabled);
-
-            if (guiBlur)
-            {
-                var cameraMain = Camera.main;
-                guiBlur.gameObject.SetActive(GameManager.inst.gameState != GameManager.State.Playing && GameManager.inst.gameState != GameManager.State.Reversing);
-                if (GameManager.inst.gameState != GameManager.State.Playing && GameManager.inst.gameState != GameManager.State.Reversing)
-                    guiBlur.transform.localPosition = new Vector3(cameraMain.transform.position.x, cameraMain.transform.position.y,
-                        (CoreConfig.Instance.IncreasedClipPlanes.Value ? -100000 : -9.9f) + 0.1f);
-            }
         }
 
         RTScaler CreateScaler(Axis axis, Color color)
@@ -130,22 +94,14 @@ namespace BetterLegacy.Core.Managers
 
         public Transform objectDragger;
 
-        public Renderer guiBlur;
-        public Image playerGUIBack;
-
-        public CanvasScaler playerGUICanvasScaler;
-
         public Image timelinePlayer;
         public Image timelineLine;
         public Image timelineLeftCap;
         public Image timelineRightCap;
         public List<Image> checkpointImages = new List<Image>();
-        public Camera perspectiveCam;
         public PostProcessLayer postProcessLayer;
         public Transform extraBG;
         public Transform video;
-        public Material bgMaterial;
-        public Image interfaceBlur;
 
         public Dictionary<string, object> assets = new Dictionary<string, object>();
     }
