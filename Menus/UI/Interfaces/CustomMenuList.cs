efb71841a2d6854170fa0074920e0700160170b7
RTMecha <mechaprower2022@gmail.com>
2024-08-18 21:11:01 +1000

    For storing multiple interfaces

---
+++ Menus/UI/Interfaces/CustomMenuList.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using SimpleJSON;
+
+namespace BetterLegacy.Menus.UI.Interfaces
+{
+    public class CustomMenuList
+    {
+        public List<MenuBase> interfaces;
+
+        public void Load(JSONNode jn)
+        {
+            interfaces.AddRange(LoadMenus(jn));
+        }
+
+        public IEnumerable<MenuBase> LoadMenus(JSONNode jn)
+        {
+            if (!jn.IsArray)
+                yield break;
+
+            for (int i = 0; i < jn.Count; i++)
+                yield return CustomMenu.Parse(jn[i]);
+        }
+    }
+}
