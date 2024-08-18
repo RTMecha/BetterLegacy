cc0b06c9b34f34d81640a95f2858f7ff332d0f67
RTMecha <mechaprower2022@gmail.com>
2024-08-18 21:10:38 +1000

    Started working on a menu prefab system

---
+++ Menus/UI/Elements/MenuPrefab.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BetterLegacy.Menus.UI.Interfaces;
+using BetterLegacy.Menus.UI.Layouts;
+using SimpleJSON;
+
+namespace BetterLegacy.Menus.UI.Elements
+{
+    public class MenuPrefab
+    {
+        public string name;
+        public string id;
+        public Dictionary<string, MenuLayoutBase> layouts = new Dictionary<string, MenuLayoutBase>();
+        public List<MenuImage> elements = new List<MenuImage>();
+
+        public static MenuPrefab Parse(JSONNode jn)
+        {
+            var menuPrefab = new MenuPrefab();
+            menuPrefab.id = jn["id"];
+            menuPrefab.name = jn["name"];
+
+            CustomMenu.ParseLayouts(menuPrefab.layouts, jn["layouts"]);
+            menuPrefab.elements.AddRange(CustomMenu.ParseElements(jn["elements"]));
+
+            return menuPrefab;
+        }
+    }
+}
