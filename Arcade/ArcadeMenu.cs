61840f4e781a54841c312cb45c1f29b61a833393
RTMecha <mechaprower2022@gmail.com>
2024-08-19 19:54:55 +1000

    Need to stop putting off the arcade rebase

---
+++ Arcade/ArcadeMenu.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using BetterLegacy.Menus;
+using BetterLegacy.Menus.UI;
+using BetterLegacy.Menus.UI.Elements;
+using BetterLegacy.Menus.UI.Layouts;
+using BetterLegacy.Menus.UI.Interfaces;
+
+namespace BetterLegacy.Arcade
+{
+    public class ArcadeMenu : MenuBase
+    {
+        public ArcadeMenu() : base(false)
+        {
+
+        }
+    }
+}
