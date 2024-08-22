3cc25bd3bee85e4e5af9e396f618efc58b7dc7ed
RTMecha <mechaprower2022@gmail.com>
2024-08-22 16:14:53 +1000

    Comment out code

--- Arcade/ArcadeMenu.cs
+++ Arcade/ArcadeMenu.cs
@@ -25,73 +25,73 @@ namespace BetterLegacy.Arcade
 
         public ArcadeMenu() : base(false)
         {
-            InterfaceManager.inst.CurrentMenu = this;
+            //InterfaceManager.inst.CurrentMenu = this;
 
-            layouts.Add("tabs", new MenuHorizontalLayout
-            {
-                name = "tabs",
-                spacing = 4f,
-                rectJSON = MenuImage.GenerateRectTransformJSON(new Vector2(0f, 470f), new Vector2(1f, 0.5f), new Vector2(0f, 0.5f), new Vector2(0.5f, 0.5f), new Vector2(-32f, 64f)),
-            });
+            //layouts.Add("tabs", new MenuHorizontalLayout
+            //{
+            //    name = "tabs",
+            //    spacing = 4f,
+            //    rectJSON = MenuImage.GenerateRectTransformJSON(new Vector2(0f, 470f), new Vector2(1f, 0.5f), new Vector2(0f, 0.5f), new Vector2(0.5f, 0.5f), new Vector2(-32f, 64f)),
+            //});
 
-            layouts.Add("levels", new MenuGridLayout
-            {
-                name = "levels",
-                spacing = new Vector2(8f, 8f),
-                cellSize = new Vector2(350f, 200f),
-                rectJSON = MenuImage.GenerateRectTransformJSON(Vector2.zero, new Vector2(0.5f, 0.5f), new Vector2(0.5f, 0.5f), new Vector2(0.5f, 0.5f), new Vector2(1800f, 800f)),
-            });
+            //layouts.Add("levels", new MenuGridLayout
+            //{
+            //    name = "levels",
+            //    spacing = new Vector2(8f, 8f),
+            //    cellSize = new Vector2(350f, 200f),
+            //    rectJSON = MenuImage.GenerateRectTransformJSON(Vector2.zero, new Vector2(0.5f, 0.5f), new Vector2(0.5f, 0.5f), new Vector2(0.5f, 0.5f), new Vector2(1800f, 800f)),
+            //});
 
-            elements.Add(new MenuButton
-            {
-                id = "0",
-                name = "close",
-                text = "<align=center><b>X",
-                parentLayout = "tabs",
-                selectionPosition = Vector2Int.zero,
-                rectJSON = MenuImage.GenerateRectTransformJSON(Vector2.zero, new Vector2(0.5f, 0.5f), new Vector2(0.5f, 0.5f), new Vector2(0.5f, 0.5f), new Vector2(78f, 78f)),
-                opacity = 0.1f,
-                val = -40f,
-                textVal = 40f,
-                selectedOpacity = 1f,
-                selectedVal = 40f,
-                selectedTextVal = -40f,
-                length = 1f,
-                playBlipSound = true,
-                func = ArcadeHelper.LoadInputSelect,
-            });
+            //elements.Add(new MenuButton
+            //{
+            //    id = "0",
+            //    name = "close",
+            //    text = "<align=center><b>X",
+            //    parentLayout = "tabs",
+            //    selectionPosition = Vector2Int.zero,
+            //    rectJSON = MenuImage.GenerateRectTransformJSON(Vector2.zero, new Vector2(0.5f, 0.5f), new Vector2(0.5f, 0.5f), new Vector2(0.5f, 0.5f), new Vector2(78f, 78f)),
+            //    opacity = 0.1f,
+            //    val = -40f,
+            //    textVal = 40f,
+            //    selectedOpacity = 1f,
+            //    selectedVal = 40f,
+            //    selectedTextVal = -40f,
+            //    length = 1f,
+            //    playBlipSound = true,
+            //    func = ArcadeHelper.LoadInputSelect,
+            //});
 
-            int num = 1;
-            for (int i = 0; i < LevelManager.Levels.Count; i++)
-            {
-                if (i >= 20)
-                    break;
+            //int num = 1;
+            //for (int i = 0; i < LevelManager.Levels.Count; i++)
+            //{
+            //    if (i >= 20)
+            //        break;
 
-                var level = LevelManager.Levels[i];
-                elements.Add(new MenuButton
-                {
-                    id = level.id,
-                    name = "level button",
-                    text = $"{level.metadata.LevelBeatmap.name}",
-                    icon = level.icon,
-                    parentLayout = "levels",
-                    selectionPosition = new Vector2Int(i % 5, num),
-                    rectJSON = MenuImage.GenerateRectTransformJSON(Vector2.zero, new Vector2(0.5f, 0.5f), new Vector2(0.5f, 0.5f), new Vector2(0.5f, 0.5f), new Vector2(64f, 64f)),
-                    opacity = 0.1f,
-                    val = -40f,
-                    textVal = -40f,
-                    selectedOpacity = 1f,
-                    selectedVal = 40f,
-                    selectedTextVal = -40f,
-                    length = 1f,
-                    playBlipSound = true,
-                    func = () => { CoreHelper.Log($"Selected level: {level}"); },
-                });
-                if ((i % 5) == 4)
-                    num++;
-            }
+            //    var level = LevelManager.Levels[i];
+            //    elements.Add(new MenuButton
+            //    {
+            //        id = level.id,
+            //        name = "level button",
+            //        text = $"{level.metadata.LevelBeatmap.name}",
+            //        icon = level.icon,
+            //        parentLayout = "levels",
+            //        selectionPosition = new Vector2Int(i % 5, num),
+            //        rectJSON = MenuImage.GenerateRectTransformJSON(Vector2.zero, new Vector2(0.5f, 0.5f), new Vector2(0.5f, 0.5f), new Vector2(0.5f, 0.5f), new Vector2(64f, 64f)),
+            //        opacity = 0.1f,
+            //        val = -40f,
+            //        textVal = -40f,
+            //        selectedOpacity = 1f,
+            //        selectedVal = 40f,
+            //        selectedTextVal = -40f,
+            //        length = 1f,
+            //        playBlipSound = true,
+            //        func = () => { CoreHelper.Log($"Selected level: {level}"); },
+            //    });
+            //    if ((i % 5) == 4)
+            //        num++;
+            //}
 
-            CoreHelper.StartCoroutine(GenerateUI());
+            //CoreHelper.StartCoroutine(GenerateUI());
         }
 
         public override void UpdateTheme()
@@ -108,7 +108,7 @@ namespace BetterLegacy.Arcade
         {
             InterfaceManager.inst.CloseMenus();
 
-            Current = new ArcadeMenu();
+            //Current = new ArcadeMenu();
         }
     }
 }
