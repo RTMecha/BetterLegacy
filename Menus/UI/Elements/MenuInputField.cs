72408f06ee966ee09637af5a7052e8536b168742
RTMecha <mechaprower2022@gmail.com>
2024-09-05 18:45:27 +1000

    Got InputField to work

--- Menus/UI/Elements/MenuInputField.cs
+++ Menus/UI/Elements/MenuInputField.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using UnityEngine;
 using UnityEngine.UI;
 
 namespace BetterLegacy.Menus.UI.Elements
@@ -15,19 +15,54 @@ namespace BetterLegacy.Menus.UI.Elements
         /// </summary>
         public InputField inputField;
 
-        public string defaultText;
+        /// <summary>
+        /// Text to display when the InputField is empty.
+        /// </summary>
+        public string placeholder;
 
+        /// <summary>
+        /// The text that has been written to the InputField.
+        /// </summary>
+        public string text;
+
+        /// <summary>
+        /// Function to run when the user is typing in the InputField.
+        /// </summary>
         public Action<string> valueChangedFunc;
+        /// <summary>
+        /// Function to run when the user is finished typing.
+        /// </summary>
         public Action<string> endEditFunc;
         public SimpleJSON.JSONNode valueChangedFuncJSON;
         public SimpleJSON.JSONNode endEditFuncJSON;
 
+        public TextAnchor textAnchor = TextAnchor.MiddleLeft;
+        public TextAnchor placeholderAnchor = TextAnchor.MiddleLeft;
+        public int textFontSize = 20;
+        public int placeholderFontSize = 20;
+
+        public UnityEngine.EventSystems.EventTrigger.Entry[] triggers;
+
+        public bool useOverrideTextColor;
+        public Color overrideTextColor;
+        public int textColor;
+        public float textHue;
+        public float textSat;
+        public float textVal;
+
+        public bool useOverridePlaceholderColor;
+        public Color overridePlaceholderColor;
+        public int placeholderColor;
+        public float placeholderHue;
+        public float placeholderSat;
+        public float placeholderVal;
+
         public void Write(string text)
         {
             valueChangedFunc?.Invoke(text);
+            this.text = text;
             if (valueChangedFuncJSON == null)
                 return;
-
             valueChangedFuncJSON["text"] = text;
             ParseFunction(valueChangedFuncJSON);
         }
@@ -35,6 +70,7 @@ namespace BetterLegacy.Menus.UI.Elements
         public void Finish(string text)
         {
             endEditFunc?.Invoke(text);
+            this.text = text;
             if (endEditFuncJSON == null)
                 return;
 
