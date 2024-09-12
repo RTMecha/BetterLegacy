953bb8a7d1ab06958581e6262deb51ac043d099e
Mecha <mechaprower2022@gmail.com>
2024-09-13 00:00:48 +1000

    Planning on an achievement editor

---
+++ Editor/Managers/AchievementEditor.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using UnityEngine;
+
+namespace BetterLegacy.Editor.Managers
+{
+    public class AchievementEditor : MonoBehaviour
+    {
+        public static AchievementEditor inst;
+
+        void Awake()
+        {
+
+        }
+    }
+}
