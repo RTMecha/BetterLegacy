df3c42bb9771d322ebe98f69b0d75ecc149c2d11
Mecha <mechaprower2022@gmail.com>
2024-09-28 02:14:19 +1000

    Version: 1.6.8

--- LegacyPlugin.cs
+++ LegacyPlugin.cs
@@ -22,7 +22,7 @@ namespace BetterLegacy
     /// <summary>
     /// Core plugin class.
     /// </summary>
-    [BepInPlugin("com.mecha.betterlegacy", "Better Legacy", "1.6.7")]
+    [BepInPlugin("com.mecha.betterlegacy", "Better Legacy", "1.6.8")]
     [BepInProcess("Project Arrhythmia.exe")]
     public class LegacyPlugin : BaseUnityPlugin
     {
