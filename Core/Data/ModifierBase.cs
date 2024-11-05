953c793f01cc7acf50ce6e630e78fec6b11da541
Mecha <mechaprower2022@gmail.com>
2024-11-06 03:14:24 +1100

    Added helper functions

--- Core/Data/ModifierBase.cs
+++ Core/Data/ModifierBase.cs
@@ -27,5 +27,75 @@ namespace BetterLegacy.Core.Data
         public float ResultTimer { get; set; }
 
         public bool hasChanged;
+
+        #region Methods
+
+        public T GetResult<T>() => (T)Result;
+
+        public bool TryGetResult<T>(out T result)
+        {
+            if (Result is T r)
+            {
+                result = r;
+                return true;
+            }
+            result = default;
+            return false;
+        }
+
+        /// <summary>
+        /// Reference value indexes here since I was pretty stupid with how I implemented the value system initially.
+        /// </summary>
+        /// <param name="index"></param>
+        /// <returns></returns>
+        public string GetValue(int index) => index == 0 ? value : commands[index];
+
+        public T GetValue<T>(int index, T defaultValue)
+        {
+            var type = typeof(T);
+            if (type == typeof(bool))
+                return (T)(object)GetBool(index, (bool)(object)defaultValue);
+            if (type == typeof(float))
+                return (T)(object)GetFloat(index, (float)(object)defaultValue);
+            if (type == typeof(int))
+                return (T)(object)GetInt(index, (int)(object)defaultValue);
+            if (type == typeof(string))
+                return (T)(object)GetString(index, (string)(object)defaultValue);
+            return default;
+        }
+
+        public bool GetBool(int index, bool defaultValue)
+        {
+            if (index < 0 || index >= commands.Count)
+                return defaultValue;
+
+            return Parser.TryParse(GetValue(index), defaultValue);
+        }
+        
+        public float GetFloat(int index, float defaultValue)
+        {
+            if (index < 0 || index >= commands.Count)
+                return defaultValue;
+
+            return Parser.TryParse(GetValue(index), defaultValue);
+        }
+        
+        public int GetInt(int index, int defaultValue)
+        {
+            if (index < 0 || index >= commands.Count)
+                return defaultValue;
+
+            return Parser.TryParse(GetValue(index), defaultValue);
+        }
+        
+        public string GetString(int index, string defaultValue)
+        {
+            if (index < 0 || index >= commands.Count)
+                return defaultValue;
+
+            return GetValue(index);
+        }
+
+        #endregion
     }
 }
