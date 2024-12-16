1aa29317fde9a6abde84ce78b75df10b2044e4de
Mecha <mechaprower2022@gmail.com>
2024-12-17 03:25:54 +1100

    Just so I don't forget to put achievements here for the next update

--- Core/Data/GameData.cs
+++ Core/Data/GameData.cs
@@ -665,6 +665,7 @@ namespace BetterLegacy.Core.Data
             beatmapData.checkpoints = orig.beatmapData.checkpoints.Select(x => BeatmapData.Checkpoint.DeepCopy(x)).ToList();
             beatmapData.markers = new List<BeatmapData.Marker>();
             beatmapData.markers.AddRange(orig.beatmapData.markers.Select(x => new Marker(x.active, x.name, x.desc, x.color, x.time)));
+            gameData.achievements = orig.achievements.Select(x => new Achievement(x.ID, x.Name, x.Description, x.Difficulty, x.Icon, x.Hidden)).ToList();
 
             gameData.beatmapData = beatmapData;
             gameData.beatmapObjects = new List<Data.BeatmapObject>((from obj in orig.beatmapObjects
@@ -1173,6 +1174,10 @@ namespace BetterLegacy.Core.Data
                     gameData.levelModifiers.Add(levelModifier);
                 }
 
+            if (jn["achievements"] != null)
+                for (int i = 0; i < jn["achievements"].Count; i++)
+                    gameData.achievements.Add(Achievement.Parse(jn["achievements"]));
+
             gameData.beatmapData = LevelBeatmapData.Parse(jn);
 
             gameData.beatmapData.markers = gameData.beatmapData.markers.OrderBy(x => x.time).ToList();
@@ -1607,6 +1612,9 @@ namespace BetterLegacy.Core.Data
                 jn["modifiers"][i]["retrigger"] = levelModifier.retriggerAmount.ToString();
             }
 
+            for (int i = 0; i < achievements.Count; i++)
+                jn["achievements"][i] = achievements[i].ToJSON();
+
             for (int i = 0; i < AssetManager.SpriteAssets.Count; i++)
             {
                 jn["assets"]["spr"][i]["n"] = AssetManager.SpriteAssets.ElementAt(i).Key;
@@ -1819,6 +1827,8 @@ namespace BetterLegacy.Core.Data
         public new List<Data.BackgroundObject> backgroundObjects = new List<Data.BackgroundObject>();
 
         public List<LevelModifier> levelModifiers = new List<LevelModifier>();
+
+        public List<Achievement> achievements = new List<Achievement>();
 
         #endregion
 
