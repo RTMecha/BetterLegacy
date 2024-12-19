1bfa7276ee303413d9e31631eb4b2ee92837ecde
Mecha <mechaprower2022@gmail.com>
2024-12-20 01:50:07 +1100

    ActiveState component for detecting if the object is active

---
+++ Components/ActiveState.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using UnityEngine;
+
+namespace BetterLegacy.Components
+{
+    public class ActiveState : MonoBehaviour
+    {
+        /// <summary>
+        /// Function to run when the object is enabled / disabled.
+        /// </summary>
+        public Action<bool> onStateChanged;
+
+        void OnEnable() => onStateChanged?.Invoke(true);
+
+        void OnDisable() => onStateChanged?.Invoke(false);
+    }
+}
