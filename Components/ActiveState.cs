684b4d38ace483acc6d6bfaef468649e140fd939
Mecha <mechaprower2022@gmail.com>
2024-12-22 14:53:56 +1100

    Added summaries and SetFunction method

--- Components/ActiveState.cs
+++ Components/ActiveState.cs
@@ -8,12 +8,21 @@ using UnityEngine;
 
 namespace BetterLegacy.Components
 {
+    /// <summary>
+    /// Runs a function when the <see cref="GameObject.activeSelf"/> state is changed.
+    /// </summary>
     public class ActiveState : MonoBehaviour
     {
         /// <summary>
         /// Function to run when the object is enabled / disabled.
         /// </summary>
         public Action<bool> onStateChanged;
+
+        /// <summary>
+        /// Sets the active state changed function.
+        /// </summary>
+        /// <param name="onStateChanged">Function to set.</param>
+        public void SetFunction(Action<bool> onStateChanged) => this.onStateChanged = onStateChanged;
 
         void OnEnable() => onStateChanged?.Invoke(true);
 
