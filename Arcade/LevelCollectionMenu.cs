a5c961e3646f694c8d3ceb917b37dd83da328f88
Mecha <mechaprower2022@gmail.com>
2024-12-30 01:13:32 +1100

    Fixed default selection being at the wrong position

--- Arcade/LevelCollectionMenu.cs
+++ Arcade/LevelCollectionMenu.cs
@@ -275,7 +275,7 @@ namespace BetterLegacy.Arcade
             exitFunc = Close;
             allowEffects = false;
             layer = 10000;
-            defaultSelection = new Vector2Int(0, 4);
+            defaultSelection = new Vector2Int(0, 1);
             InterfaceManager.inst.SetCurrentInterface(this);
         }
 
