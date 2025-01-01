2c4c47be509ef30cec1ba9bd4a01d487c4cdf0fd
Mecha <mechaprower2022@gmail.com>
2025-01-02 09:37:19 +1100

    Probably gonna do a current dialog system like this

--- Editor/Data/EditorDialog.cs
+++ Editor/Data/EditorDialog.cs
@@ -12,10 +12,18 @@ namespace BetterLegacy.Editor.Data
     // todo: replace original editor dialog with this
     public class EditorDialog : Exists
     {
+        #region Properties
+
+        /// <summary>
+        /// The editor dialog that is currently being viewed.
+        /// </summary>
+        public static EditorDialog CurrentDialog { get; set; }
+
         /// <summary>
         /// Name of the editor dialog.
         /// </summary>
         public string Name { get; set; }
+
         /// <summary>
         /// Game object of the editor dialog.
         /// </summary>
@@ -30,5 +38,40 @@ namespace BetterLegacy.Editor.Data
         /// Title of the editor dialog.
         /// </summary>
         public Text Title { get; set; }
+
+        #endregion
+
+        #region Methods
+
+        /// <summary>
+        /// Opens the editor dialog and sets it as the current dialog.
+        /// </summary>
+        public void Open()
+        {
+            CurrentDialog?.Close();
+            CurrentDialog = this;
+            SetActive(true);
+        }
+
+        /// <summary>
+        /// Closes the editor dialog.
+        /// </summary>
+        public void Close()
+        {
+            CurrentDialog = null;
+            SetActive(false);
+        }
+
+        /// <summary>
+        /// Sets the active state of the editor dialog.
+        /// </summary>
+        /// <param name="active">The active state to set.</param>
+        public void SetActive(bool active)
+        {
+            if (GameObject)
+                GameObject.SetActive(active);
+        }
+
+        #endregion
     }
 }
