566c4c4ff0957c68e353ee221a1f25de4090a03c
Mecha <mechaprower2022@gmail.com>
2025-01-02 09:37:37 +1100

    Assign from a game object

--- Editor/Data/EditorPopup.cs
+++ Editor/Data/EditorPopup.cs
@@ -18,6 +18,21 @@ namespace BetterLegacy.Editor.Data
     /// </summary>
     public class EditorPopup
     {
+        public EditorPopup() { }
+
+        public EditorPopup(string name) => Name = name;
+
+        public EditorPopup(string name, string title, Vector2 defaultPosition, Vector2 size, Action<string> refreshSearch = null, Action close = null, string placeholderText = "Search...")
+        {
+            Name = name;
+            this.title = title;
+            this.defaultPosition = defaultPosition;
+            this.size = size;
+            this.refreshSearch = refreshSearch;
+            this.close = close;
+            this.placeholderText = placeholderText;
+        }
+
         #region Properties
 
         /// <summary>
@@ -54,6 +69,11 @@ namespace BetterLegacy.Editor.Data
         public Text Title { get; set; }
 
         /// <summary>
+        /// Scrollbar of the editor popups' content.
+        /// </summary>
+        public Scrollbar ContentScrollbar { get; set; }
+
+        /// <summary>
         /// If the editor popup is open.
         /// </summary>
         public bool IsOpen => GameObject && GameObject.activeInHierarchy;
@@ -62,11 +82,67 @@ namespace BetterLegacy.Editor.Data
 
         #region Fields
 
+        /// <summary>
+        /// Animation of the editor popup.
+        /// </summary>
         public EditorAnimation editorAnimation;
+
+        /// <summary>
+        /// Title of the editor popup.
+        /// </summary>
+        public string title;
+
+        /// <summary>
+        /// Default position the editor popup should be at.
+        /// </summary>
+        public Vector2 defaultPosition;
+
+        /// <summary>
+        /// Size of the editor popup.
+        /// </summary>
+        public Vector2 size;
+
+        /// <summary>
+        /// Refresh search function.
+        /// </summary>
+        public Action<string> refreshSearch;
+
+        /// <summary>
+        /// Close editor popup function.
+        /// </summary>
+        public Action close;
+
+        /// <summary>
+        /// The placeholder string of the editor popups' search field.
+        /// </summary>
+        public string placeholderText = "Search...";
 
         #endregion
 
         #region Methods
+
+        /// <summary>
+        /// Creates an editor popup from an existing popup game object.
+        /// </summary>
+        /// <param name="name">Name of the popup.</param>
+        /// <param name="gameObject">Game object reference.</param>
+        /// <returns>Returns an editor popup made from a game object.</returns>
+        public static EditorPopup FromGameObject(string name, GameObject gameObject)
+        {
+            var editorPopup = new EditorPopup(name);
+            editorPopup.Assign(gameObject);
+
+            try
+            {
+                if (editorPopup.Title)
+                    editorPopup.title = editorPopup.Title.text;
+                if (editorPopup.SearchField)
+                    editorPopup.placeholderText = editorPopup.SearchField.GetPlaceholderText().text;
+            }
+            catch { }
+
+            return editorPopup;
+        }
 
         /// <summary>
         /// Opens the editor popup.
@@ -78,6 +154,10 @@ namespace BetterLegacy.Editor.Data
         /// </summary>
         public void Close() => PlayAnimation(false);
 
+        /// <summary>
+        /// Plays the popups' animation.
+        /// </summary>
+        /// <param name="active"></param>
         public void PlayAnimation(bool active)
         {
             var play = EditorConfig.Instance.PlayEditorAnimations.Value;
@@ -89,7 +169,7 @@ namespace BetterLegacy.Editor.Data
             {
                 if (!editorAnimation.Active)
                 {
-                    GameObject.SetActive(active);
+                    SetActive(active);
 
                     return;
                 }
@@ -173,7 +253,7 @@ namespace BetterLegacy.Editor.Data
 
                 animation.onComplete = () =>
                 {
-                    dialog.gameObject.SetActive(active);
+                    SetActive(active);
 
                     if (editorAnimation.PosActive)
                         dialog.localPosition = new Vector3(active ? editorAnimation.PosEnd.x : editorAnimation.PosStart.x, active ? editorAnimation.PosEnd.y : editorAnimation.PosStart.y, 0f);
@@ -189,79 +269,188 @@ namespace BetterLegacy.Editor.Data
             }
 
             if (!play || !editorAnimation || active)
+                SetActive(active);
+        }
+
+        /// <summary>
+        /// Sets the editor popup active state.
+        /// </summary>
+        /// <param name="active">Active state to set.</param>
+        public void SetActive(bool active)
+        {
+            if (GameObject)
                 GameObject.SetActive(active);
         }
 
-        public void Assign(string name, GameObject gameObject, Button close, InputField searchField, Transform content, GridLayoutGroup grid, RectTransform topPanel, Text title)
+        /// <summary>
+        /// Assigns the elements of a game object to this editor popup.
+        /// </summary>
+        /// <param name="popup">Poppup game object to assign from.</param>
+        public void Assign(GameObject popup)
         {
-            Name = name;
-            GameObject = gameObject;
-            CloseButton = close;
-            SearchField = searchField;
-            Content = content;
-            Grid = grid;
-            TopPanel = topPanel;
-            Title = title;
+            GameObject = popup;
+            if (popup.transform.TryFind("Panel", out Transform topPanel))
+            {
+                TopPanel = topPanel.AsRT();
+                if (topPanel.TryFind("Text", out Transform title) && title.gameObject.TryGetComponent(out Text titleText))
+                    Title = titleText;
+
+                if (topPanel.TryFind("x", out Transform close) && close.gameObject.TryGetComponent(out Button closeButton))
+                    CloseButton = closeButton;
+            }
+
+            if (popup.transform.TryFind("search-box/search", out Transform searchBox) && searchBox.gameObject.TryGetComponent(out InputField searchField))
+                SearchField = searchField;
+
+            if (popup.transform.TryFind("mask/content", out Transform content))
+            {
+                Content = content;
+                Grid = content.GetComponent<GridLayoutGroup>();
+            }
+
+            if (popup.transform.TryFind("Scrollbar", out Transform sidebar) && sidebar.gameObject.TryGetComponent(out Scrollbar contentScrollbar))
+                ContentScrollbar = contentScrollbar;
         }
 
-        public void Init(string name, string title, Vector2 defaultPosition, Vector2 size, Action<string> refreshSearch = null, Action close = null, string placeholderText = "Search...")
+        /// <summary>
+        /// Initializes the editor popup.
+        /// </summary>
+        public void Init()
         {
-            Name = name;
-
+            var name = Name;
             var popup = GameObject;
             if (popup)
                 CoreHelper.Destroy(popup);
 
             popup = EditorManager.inst.GetDialog("Parent Selector").Dialog.gameObject.Duplicate(RTEditor.inst.popups, name);
-            GameObject = popup;
+            Assign(popup);
             popup.transform.localPosition = Vector3.zero;
-
-            var inSize = size == Vector2.zero ? new Vector2(600f, 450f) : size;
-            popup.transform.AsRT().anchoredPosition = defaultPosition;
-            popup.transform.AsRT().sizeDelta = inSize;
-            TopPanel = popup.transform.Find("Panel").AsRT();
-            TopPanel.sizeDelta = new Vector2(inSize.x + 32f, 32f);
-            var text = TopPanel.Find("Text").GetComponent<Text>();
-            Title = text;
-            text.text = title;
-
-            popup.transform.Find("search-box").AsRT().sizeDelta = new Vector2(inSize.x, 32f);
-            Grid = popup.transform.Find("mask/content").GetComponent<GridLayoutGroup>();
-            Grid.cellSize = new Vector2(inSize.x - 5f, 32f);
-            popup.transform.Find("Scrollbar").AsRT().sizeDelta = new Vector2(32f, inSize.y);
-
-            CloseButton = TopPanel.Find("x").GetComponent<Button>();
-            CloseButton.onClick.ClearAll();
-            CloseButton.onClick.AddListener(() =>
-            {
-                EditorManager.inst.HideDialog(name);
-                close?.Invoke();
-            });
-
-            SearchField = popup.transform.Find("search-box/search").GetComponent<InputField>();
-            SearchField.onValueChanged.ClearAll();
-            SearchField.onValueChanged.AddListener(_val => refreshSearch?.Invoke(_val));
-            SearchField.GetPlaceholderText().text = placeholderText;
-            Content = popup.transform.Find("mask/content");
 
             EditorHelper.AddEditorPopup(name, popup);
 
             EditorThemeManager.AddGraphic(popup.GetComponent<Image>(), ThemeGroup.Background_1, true, roundedSide: SpriteHelper.RoundedSide.Bottom_Left_I);
-
             EditorThemeManager.AddGraphic(TopPanel.GetComponent<Image>(), ThemeGroup.Background_1, true, roundedSide: SpriteHelper.RoundedSide.Top);
-
             EditorThemeManager.AddSelectable(CloseButton, ThemeGroup.Close);
-
             EditorThemeManager.AddGraphic(CloseButton.transform.GetChild(0).GetComponent<Image>(), ThemeGroup.Close_X);
+            EditorThemeManager.AddLightText(Title);
+            EditorThemeManager.AddScrollbar(ContentScrollbar, scrollbarRoundedSide: SpriteHelper.RoundedSide.Bottom_Right_I);
+            EditorThemeManager.AddInputField(SearchField, ThemeGroup.Search_Field_1, 1, SpriteHelper.RoundedSide.Bottom);
 
-            EditorThemeManager.AddLightText(text);
-
-            var scrollbar = popup.transform.Find("Scrollbar").GetComponent<Scrollbar>();
-            scrollbar.value = 1f;
-            EditorThemeManager.AddScrollbar(scrollbar, scrollbarRoundedSide: SpriteHelper.RoundedSide.Bottom_Right_I);
-
-            EditorThemeManager.AddInputField(popup.transform.Find("search-box/search").GetComponent<InputField>(), ThemeGroup.Search_Field_1, 1, SpriteHelper.RoundedSide.Bottom);
+            Render();
         }
+
+        /// <summary>
+        /// Renders the whole editor popup.
+        /// </summary>
+        public void Render()
+        {
+            RenderTitle();
+            RenderSize();
+            RenderPlaceholderText();
+            UpdateSearchFunction(refreshSearch);
+            UpdateCloseFunction(close);
+        }
+
+        /// <summary>
+        /// Renders the editor popup title.
+        /// </summary>
+        public void RenderTitle() => RenderTitle(title);
+
+        /// <summary>
+        /// Renders the editor popup title.
+        /// </summary>
+        /// <param name="title">Title of the editor popup.</param>
+        public void RenderTitle(string title)
+        {
+            if (Title)
+                Title.text = title;
+        }
+
+        /// <summary>
+        /// Renders the editor popup position and size.
+        /// </summary>
+        public void RenderSize() => RenderSize(size);
+
+        /// <summary>
+        /// Renders the editor popup position and size.
+        /// </summary>
+        /// <param name="size">Size of the editor popup.</param>
+        public void RenderSize(Vector2 size)
+        {
+            if (!GameObject)
+                return;
+
+            var inSize = size == Vector2.zero ? new Vector2(600f, 450f) : size;
+            GameObject.transform.AsRT().anchoredPosition = defaultPosition;
+            GameObject.transform.AsRT().sizeDelta = inSize;
+            if (TopPanel)
+                TopPanel.sizeDelta = new Vector2(inSize.x + 32f, 32f);
+
+            if (SearchField)
+                SearchField.gameObject.transform.parent.AsRT().sizeDelta = new Vector2(inSize.x, 32f);
+
+            if (Grid)
+                Grid.cellSize = new Vector2(inSize.x - 5f, 32f);
+
+            if (ContentScrollbar)
+            {
+                ContentScrollbar.value = 1f;
+                ContentScrollbar.gameObject.transform.AsRT().sizeDelta = new Vector2(32f, inSize.y);
+            }
+        }
+
+        /// <summary>
+        /// Renders the editor popups' search field placeholder.
+        /// </summary>
+        public void RenderPlaceholderText() => RenderPlaceholderText(placeholderText);
+
+        /// <summary>
+        /// Renders the editor popups' search field placeholder.
+        /// </summary>
+        /// <param name="text">Text to set.</param>
+        public void RenderPlaceholderText(string text)
+        {
+            if (SearchField)
+                SearchField.GetPlaceholderText().text = text;
+        }
+
+        /// <summary>
+        /// Updates the search field.
+        /// </summary>
+        /// <param name="onSearch">Runs when the user types in the search field.</param>
+        public void UpdateSearchFunction(Action<string> onSearch)
+        {
+            SearchField.onValueChanged.ClearAll();
+            SearchField.onValueChanged.AddListener(_val => onSearch?.Invoke(_val));
+        }
+
+        /// <summary>
+        /// Updates the search field.
+        /// </summary>
+        /// <param name="searchTerm">What has been searched.</param>
+        /// <param name="onSearch">Runs when the user types in the search field.</param>
+        public void UpdateSearchFunction(string searchTerm, Action<string> onSearch)
+        {
+            SearchField.onValueChanged.ClearAll();
+            SearchField.text = searchTerm;
+            SearchField.onValueChanged.AddListener(_val => onSearch?.Invoke(_val));
+        }
+
+        /// <summary>
+        /// Updates the close button.
+        /// </summary>
+        /// <param name="close">Runs when the user clicks the close popup button.</param>
+        public void UpdateCloseFunction(Action close)
+        {
+            CloseButton.onClick.ClearAll();
+            CloseButton.onClick.AddListener(() =>
+            {
+                EditorManager.inst.HideDialog(Name);
+                close?.Invoke();
+            });
+        }
+
+        public override string ToString() => $"{Name} - {title}";
 
         #endregion
     }
