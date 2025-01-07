c8d9826542fde1aea4d62fd09c8f3f9eff852afc
Mecha <mechaprower2022@gmail.com>
2025-01-08 00:15:28 +1100

    Changed notifier to only change transparency instead of active state

--- Editor/Components/ModifierActiveNotifier.cs
+++ Editor/Components/ModifierActiveNotifier.cs
@@ -2,18 +2,20 @@
 using UnityEngine;
 
 using BetterLegacy.Core.Data;
+using UnityEngine.UI;
+using LSFunctions;
 
 namespace BetterLegacy.Editor.Components
 {
     public class ModifierActiveNotifier : MonoBehaviour
     {
         public ModifierBase modifierBase;
-        public GameObject notifier;
+        public Image notifier;
 
         void Update()
         {
             if (notifier && modifierBase != null)
-                notifier.SetActive(modifierBase.type == ModifierBase.Type.Action ? modifierBase.running : modifierBase.triggered);
+                notifier.color = LSColors.fadeColor(notifier.color,( modifierBase.type == ModifierBase.Type.Action ? modifierBase.running : modifierBase.triggered) ? 1f : 0.1f);
         }
     }
 }
