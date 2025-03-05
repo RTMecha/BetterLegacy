99956707b4ec2da9071ec2c6ae7fa4328f641b9f
RTMecha <125487712+RTMecha@users.noreply.github.com>
2025-03-05 18:58:51 +1100

    Timer struct

---
+++ Core/Data/RTTimer.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+
+namespace BetterLegacy.Core.Data
+{
+    /// <summary>
+    /// Represents a Unity timer.
+    /// </summary>
+    public struct RTTimer
+    {
+        public RTTimer(float time = 0f, float timeOffset = 0f)
+        {
+            this.time = time;
+            this.timeOffset = timeOffset;
+            UpdateTimeOffset();
+        }
+
+        /// <summary>
+        /// Time since start.
+        /// </summary>
+        public float time;
+        float timeOffset;
+
+        /// <summary>
+        /// Resets the timer.
+        /// </summary>
+        public void UpdateTimeOffset() => timeOffset = Time.time;
+
+        /// <summary>
+        /// Updates the timer.
+        /// </summary>
+        public void Update() => time = Time.time - timeOffset;
+
+        public override string ToString() => time.ToString();
+    }
+}
