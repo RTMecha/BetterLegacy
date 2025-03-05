ce71ae77dbe6bc1b8c8a269fe5a780b12aacb2e9
RTMecha <125487712+RTMecha@users.noreply.github.com>
2025-03-05 21:08:30 +1100

    Throw null reference

---
+++ Core/Helpers/ExceptionHelper.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BetterLegacy.Core.Helpers
+{
+    public static class ExceptionHelper
+    {
+        public static void NullReference(object obj, string name)
+        {
+            if (obj == null)
+                throw new NullReferenceException($"{name} is null.");
+        }
+    }
+}
