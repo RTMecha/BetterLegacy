a91f77427e9028711a6778da6591b08b89b9c47d
RTMecha <125487712+RTMecha@users.noreply.github.com>
2025-03-11 16:21:03 +1100

    Rework to no longer use the vanilla prefab type since it's unnecessary now

--- Core/Data/Beatmap/PrefabType.cs
+++ Core/Data/Beatmap/PrefabType.cs
@@ -3,90 +3,150 @@ using LSFunctions;
 using SimpleJSON;
 using System.Collections.Generic;
 using UnityEngine;
-using BasePrefabType = DataManager.PrefabType;
 
 namespace BetterLegacy.Core.Data.Beatmap
 {
-    public class PrefabType : BasePrefabType
+    /// <summary>
+    /// Represents the type of a prefab.
+    /// </summary>
+    public class PrefabType : Exists
     {
         public PrefabType(string name, Color color)
         {
-            Name = name;
-            Color = color;
+            this.name = name;
+            this.color = color;
             id = LSText.randomNumString(16);
         }
 
+        #region Values
+
+        /// <summary>
+        /// Identification of the prefab type.
+        /// </summary>
         public string id;
 
+        /// <summary>
+        /// Name of the prefab type.
+        /// </summary>
+        public string name;
+
+        /// <summary>
+        /// Color to apply to prefabs using this type.
+        /// </summary>
+        public Color color;
+
+        /// <summary>
+        /// Icon to apply to prefabs using this type.
+        /// </summary>
         public Sprite icon;
 
+        /// <summary>
+        /// Path to the prefab type's file.
+        /// </summary>
         public string filePath;
+
+        #endregion
+
+        #region Defaults
+
+        static PrefabType invalidType;
+
+        /// <summary>
+        /// The invalid prefab type if no prefab types were found.
+        /// </summary>
+        public static PrefabType InvalidType
+        {
+            get
+            {
+                if (!invalidType)
+                    invalidType = new PrefabType("invalid", Color.red) { icon = LegacyPlugin.AtanPlaceholder };
+                return invalidType;
+            }
+        }
 
         public bool isDefault;
 
+        public const string BOMBS = "7171177971767435";
+        public const string BULLETS = "7177271762800247";
+        public const string BEAMS = "9453916730542677";
+        public const string SPINNERS = "1644373336900132";
+        public const string PULSES = "4045878933744147";
+        public const string CHARACTERS = "4965490066612888";
+        public const string CHARACTER_PARTS = "9570474999546241";
+        public const string PROPS = "5992953518858497";
+        public const string STATIC = "0689518742909570";
+        public const string MISC_1 = "0236192376944164";
+        public const string MISC_2 = "8073495542187348";
+        public const string MISC_3 = "5347376169954072";
+        public const string MISC_4 = "4727401743041957";
+
         public static Dictionary<int, string> prefabTypeLSIndexToID = new Dictionary<int, string>()
         {
-            { 0, "7171177971767435" }, // Bombs
-            { 1, "7177271762800247" }, // Bullets
-            { 2, "9453916730542677" }, // Beams
-            { 3, "1644373336900132" }, // Spinners
-            { 4, "4045878933744147" }, // Pulses
-            { 5, "4965490066612888" }, // Characters
-            { 6, "0236192376944164" }, // Misc 1
-            { 7, "8073495542187348" }, // Misc 2
-            { 8, "5347376169954072" }, // Misc 3
-            { 9, "4727401743041957" }, // Misc 4
+            { 0, BOMBS }, // Bombs
+            { 1, BULLETS }, // Bullets
+            { 2, BEAMS }, // Beams
+            { 3, SPINNERS }, // Spinners
+            { 4, PULSES }, // Pulses
+            { 5, CHARACTERS }, // Characters
+            { 6, MISC_1 }, // Misc 1
+            { 7, MISC_2 }, // Misc 2
+            { 8, MISC_3 }, // Misc 3
+            { 9, MISC_4 }, // Misc 4
         };
 
         public static Dictionary<int, string> prefabTypeVGIndexToID = new Dictionary<int, string>()
         {
-            { 0, "4965490066612888" }, // Characters
-            { 1, "9570474999546241" }, // Character Parts
-            { 2, "5992953518858497" }, // Props
-            { 3, "7177271762800247" }, // Bullets
-            { 4, "4045878933744147" }, // Pulses
-            { 5, "7171177971767435" }, // Bombs
-            { 6, "1644373336900132" }, // Spinners
-            { 7, "9453916730542677" }, // Beams
-            { 8, "0689518742909570" }, // Static
-            { 9, "0236192376944164" }, // Misc 1
-            { 10, "8073495542187348" }, // Misc 2
-            { 11, "5347376169954072" }, // Misc 3
+            { 0, CHARACTERS }, // Characters
+            { 1, CHARACTER_PARTS }, // Character Parts
+            { 2, PROPS }, // Props
+            { 3, BULLETS }, // Bullets
+            { 4, PULSES }, // Pulses
+            { 5, BOMBS }, // Bombs
+            { 6, SPINNERS }, // Spinners
+            { 7, BEAMS }, // Beams
+            { 8, STATIC }, // Static
+            { 9, MISC_1 }, // Misc 1
+            { 10, MISC_2 }, // Misc 2
+            { 11, MISC_3 }, // Misc 3
         };
 
         public static Dictionary<string, int> prefabTypeLSIDToIndex = new Dictionary<string, int>()
         {
-            { "7171177971767435", 0 }, // Bombs
-            { "7177271762800247", 1 }, // Bullets
-            { "9453916730542677", 2 }, // Beams
-            { "1644373336900132", 3 }, // Spinners
-            { "4045878933744147", 4 }, // Pulses
-            { "4965490066612888", 5 }, // Characters
-            { "0236192376944164", 6 }, // Misc 1
-            { "8073495542187348", 7 }, // Misc 2
-            { "5347376169954072", 8 }, // Misc 3
-            { "4727401743041957", 9 }, // Misc 4
+            { BOMBS, 0 }, // Bombs
+            { BULLETS, 1 }, // Bullets
+            { BEAMS, 2 }, // Beams
+            { SPINNERS, 3 }, // Spinners
+            { PULSES, 4 }, // Pulses
+            { CHARACTERS, 5 }, // Characters
+            { MISC_1, 6 }, // Misc 1
+            { MISC_2, 7 }, // Misc 2
+            { MISC_3, 8 }, // Misc 3
+            { MISC_4, 9 }, // Misc 4
         };
 
         public static Dictionary<string, int> prefabTypeVGIDToIndex = new Dictionary<string, int>()
         {
-            { "4965490066612888", 0 }, // Characters
-            { "9570474999546241", 1 }, // Character Parts
-            { "5992953518858497", 2 }, // Props
-            { "7177271762800247", 3 }, // Bullets
-            { "4045878933744147", 4 }, // Pulses
-            { "7171177971767435", 5 }, // Bombs
-            { "1644373336900132", 6 }, // Spinners
-            { "9453916730542677", 7 }, // Beams
-            { "0689518742909570", 8 }, // Static
-            { "0236192376944164", 9 }, // Misc 1
-            { "8073495542187348", 10 }, // Misc 2
-            { "5347376169954072", 11 }, // Misc 3
+            { CHARACTERS, 0 }, // Characters
+            { CHARACTER_PARTS, 1 }, // Character Parts
+            { PROPS, 2 }, // Props
+            { BULLETS, 3 }, // Bullets
+            { PULSES, 4 }, // Pulses
+            { BOMBS, 5 }, // Bombs
+            { SPINNERS, 6 }, // Spinners
+            { BEAMS, 7 }, // Beams
+            { STATIC, 8 }, // Static
+            { MISC_1, 9 }, // Misc 1
+            { MISC_2, 10 }, // Misc 2
+            { MISC_3, 11 }, // Misc 3
         };
 
-        public void AssignColor(string _val) => Color = _val.Length == 8 ? LSColors.HexToColorAlpha(_val) : _val.Length == 6 ? LSColors.HexToColor(_val) : LSColors.pink500;
+        #endregion
 
-        public static PrefabType DeepCopy(PrefabType og, bool newID = true) => new PrefabType(og.Name, og.Color)
+        #region Methods
+
+        public void AssignColor(string _val) => color = _val.Length == 8 ? LSColors.HexToColorAlpha(_val) : _val.Length == 6 ? LSColors.HexToColor(_val) : LSColors.pink500;
+
+        public static PrefabType DeepCopy(PrefabType og, bool newID = true) => new PrefabType(og.name, og.color)
         {
             id = newID ? LSText.randomNumString(16) : og.id,
             icon = og.icon,
@@ -108,11 +168,12 @@ namespace BetterLegacy.Core.Data.Beatmap
 
             return prefabType;
         }
+
         public JSONNode ToJSON()
         {
             var jn = JSON.Parse("{}");
-            jn["name"] = Name;
-            jn["color"] = CoreHelper.ColorToHex(Color);
+            jn["name"] = name;
+            jn["color"] = CoreHelper.ColorToHex(color);
             jn["id"] = id;
 
             try
@@ -128,23 +189,13 @@ namespace BetterLegacy.Core.Data.Beatmap
             return jn;
         }
 
-        static PrefabType invalidType;
-        public static PrefabType InvalidType
-        {
-            get
-            {
-                if (!invalidType)
-                    invalidType = new PrefabType("invalid", Color.red) { icon = LegacyPlugin.AtanPlaceholder };
-                return invalidType;
-            }
-        }
-
+        #endregion
 
         #region Operators
 
         public static implicit operator bool(PrefabType exists) => exists != null;
 
-        public override string ToString() => Name;
+        public override string ToString() => name;
 
         #endregion
     }
