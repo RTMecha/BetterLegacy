2808c45b5fea588e4c2423e0ede8926084b76856
RTMecha <125487712+RTMecha@users.noreply.github.com>
2025-03-15 15:31:53 +1100

    Not sure if I'll end up going with this or just keep doing it as usual

---
+++ Core/Data/PAObject.cs
@@ -0,0 +1,17 @@
+﻿using SimpleJSON;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BetterLegacy.Core.Data
+{
+    public abstract class PAObject<T> : Exists
+    {
+        public PAObject() { }
+
+        public abstract T Parse(JSONNode jn, ArrhythmiaType type = ArrhythmiaType.LS);
+        public abstract JSONNode ToJSON(ArrhythmiaType type = ArrhythmiaType.LS);
+    }
+}
