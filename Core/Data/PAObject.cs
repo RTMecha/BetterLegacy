41e74f4d6e61af3d6bbf0648425954a0bf12573d
RTMecha <125487712+RTMecha@users.noreply.github.com>
2025-03-25 04:33:52 +1100

    Continue work on base PA object

--- Core/Data/PAObject.cs
+++ Core/Data/PAObject.cs
@@ -1,12 +1,37 @@
-﻿using SimpleJSON;
+﻿using System.Runtime.CompilerServices;
+
+using LSFunctions;
+
+using SimpleJSON;
 
 namespace BetterLegacy.Core.Data
 {
+    /// <summary>
+    /// Base object used for PA Objects.
+    /// </summary>
+    /// <typeparam name="T">Type of the PA Object.</typeparam>
     public abstract class PAObject<T> : Exists
     {
         public PAObject() { }
 
-        public abstract T Parse(JSONNode jn, ArrhythmiaType type = ArrhythmiaType.LS);
-        public abstract JSONNode ToJSON(ArrhythmiaType type = ArrhythmiaType.LS);
+        public string id = LSText.randomString(16);
+
+        public virtual T ReadJSONVG(JSONNode jn) => default;
+
+        public virtual T ReadJSON(JSONNode jn) => default;
+
+        public virtual JSONNode ToJSONVG() => JSON.Parse("{}");
+
+        public virtual JSONNode ToJSON() => JSON.Parse("{}");
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static bool operator ==(PAObject<T> a, PAObject<T> b) => a && a.Equals(b);
+        
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static bool operator !=(PAObject<T> a, PAObject<T> b) => !a || !a.Equals(b);
+
+        public override bool Equals(object obj) => obj is PAObject<T> paObj && id == paObj.id;
+
+        public override int GetHashCode() => base.GetHashCode();
     }
 }
