4128da0825e8dc06bad9750ff8f6837af9287725
RTMecha <125487712+RTMecha@users.noreply.github.com>
2025-04-11 12:32:09 +1000

    Use base PAObject

--- Core/Data/Achievement.cs
+++ Core/Data/Achievement.cs
@@ -15,45 +15,48 @@ namespace BetterLegacy.Core.Data
     /// <summary>
     /// Custom achievement class to be used for levels and the game.
     /// </summary>
-    public class Achievement
+    public class Achievement : PAObject<Achievement>
     {
-        public Achievement(string id, string name, string description, int difficulty, Sprite icon, bool hidden = false)
+        public Achievement()
         {
-            ID = id;
-            Name = name;
-            Description = description;
-            Difficulty = difficulty;
-            Icon = icon;
-            Hidden = hidden;
-            unlocked = false;
+            id = GetNumberID();
         }
 
-        public string ID { get; set; }
+        public Achievement(string id, string name, string description, int difficulty, Sprite icon, bool hidden = false)
+        {
+            this.id = id;
+            this.name = name;
+            this.description = description;
+            this.difficulty = difficulty;
+            this.icon = icon;
+            this.hidden = hidden;
+            unlocked = false;
+        }
 
         /// <summary>
         /// Name of the achievement.
         /// </summary>
-        public string Name { get; set; }
+        public string name;
 
         /// <summary>
         /// Short description on how to get the achievement (or not if achievement is hidden).
         /// </summary>
-        public string Description { get; set; }
+        public string description;
 
         /// <summary>
         /// Icon the achievement popup shows.
         /// </summary>
-        public Sprite Icon { get; set; }
+        public Sprite icon;
 
         /// <summary>
         /// Difficulty of the achievement. Based on metadata difficulty.
         /// </summary>
-        public int Difficulty { get; set; }
+        public int difficulty;
 
         /// <summary>
         /// If the achievement shows up in achievement lists when not unlocked.
         /// </summary>
-        public bool Hidden { get; set; }
+        public bool hidden;
 
         /// <summary>
         /// If the achievement has already been achieved or not.
@@ -63,7 +66,7 @@ namespace BetterLegacy.Core.Data
         /// <summary>
         /// The metadata difficulty type.
         /// </summary>
-        public DataManager.Difficulty DifficultyType => Difficulty == 0 ? DataManager.inst.difficulties.Last() : Difficulty - 1 >= 0 && Difficulty - 1 < DataManager.inst.difficulties.Count ? DataManager.inst.difficulties[Difficulty - 1] : new DataManager.Difficulty("Unknown Difficulty", Color.red);
+        public DataManager.Difficulty DifficultyType => difficulty == 0 ? DataManager.inst.difficulties.Last() : difficulty - 1 >= 0 && difficulty - 1 < DataManager.inst.difficulties.Count ? DataManager.inst.difficulties[difficulty - 1] : new DataManager.Difficulty("Unknown Difficulty", Color.red);
 
         public void Unlock()
         {
@@ -71,7 +74,7 @@ namespace BetterLegacy.Core.Data
                 return;
 
             unlocked = true;
-            CoreHelper.Log($"{Name} achieved!");
+            CoreHelper.Log($"{name} achieved!");
         }
 
         public void Lock()
@@ -82,40 +85,43 @@ namespace BetterLegacy.Core.Data
             unlocked = false;
         }
 
-        public static void Test()
+        public override void CopyData(Achievement orig, bool newID = true)
         {
-            var achievement = TestAchievement;
-
-            achievement.Unlock();
+            id = newID ? GetNumberID() : orig.id;
+            name = orig.name;
+            description = orig.description;
+            difficulty = orig.difficulty;
+            icon = orig.icon;
+            hidden = orig.hidden;
+            unlocked = orig.unlocked;
         }
 
-        public static Achievement DeepCopy(Achievement orig, bool newID = true) => new Achievement(newID ? LSText.randomNumString(16) : orig.ID, orig.Name, orig.Description, orig.Difficulty, orig.Icon, orig.Hidden);
+        public override void ReadJSON(JSONNode jn)
+        {
+            id = jn["id"];
+            name = jn["name"];
+            difficulty = jn["difficulty"].AsInt;
+            icon = SpriteHelper.StringToSprite(jn["icon"]);
+            hidden = jn["hidden"].AsBool;
+        }
 
-        public static Achievement Parse(JSONNode jn, bool parseUnlock = false)
-            => new Achievement(jn["id"], jn["name"], jn["desc"], jn["difficulty"].AsInt, SpriteHelper.StringToSprite(jn["icon"]), jn["hidden"].AsBool)
-            {
-                unlocked = parseUnlock && jn["unlocked"] != null && jn["unlocked"].AsBool,
-            };
-
-        public JSONNode ToJSON(bool saveUnlock = false)
+        public override JSONNode ToJSON()
         {
             var jn = JSON.Parse("{}");
 
-            jn["id"] = ID;
-            jn["name"] = Name;
-            jn["desc"] = Description;
-            jn["difficulty"] = Difficulty.ToString();
-            jn["hidden"] = Hidden.ToString();
-            if (saveUnlock)
-                jn["unlocked"] = unlocked.ToString();
+            jn["id"] = id;
+            jn["name"] = name;
+            jn["desc"] = description;
+            jn["difficulty"] = difficulty.ToString();
+            jn["hidden"] = hidden.ToString();
 
-            jn["icon"] = SpriteHelper.SpriteToString(Icon);
+            jn["icon"] = SpriteHelper.SpriteToString(icon);
 
             return jn;
         }
 
         public static Achievement TestAchievement => new Achievement("265265", "Test", "Test this achievement!", 3, LegacyPlugin.AtanPlaceholder);
 
-        public override string ToString() => $"{ID} - {Name} = {unlocked}";
+        public override string ToString() => $"{id} - {name} = {unlocked}";
     }
 }
