8aae1fc28f8ea82e55183b168e4ab3d0ac96294e
RTMecha <125487712+RTMecha@users.noreply.github.com>
2025-04-25 19:43:00 +1000

    Removed unnecessary code

--- Patchers/MenuPatchers/InterfaceLoaderPatch.cs
+++ Patchers/MenuPatchers/InterfaceLoaderPatch.cs
@@ -1,14 +1,4 @@
-﻿using System.IO;
-
-using UnityEngine;
-
-using HarmonyLib;
-
-using LSFunctions;
-
-using YamlDotNet.Serialization;
-
-using BetterLegacy.Core.Helpers;
+﻿using HarmonyLib;
 
 namespace BetterLegacy.Patchers
 {
@@ -17,37 +7,6 @@ namespace BetterLegacy.Patchers
     {
         [HarmonyPatch(nameof(InterfaceLoader.Start))]
         [HarmonyPrefix]
-        static bool InterfaceLoaderPrefix(InterfaceLoader __instance)
-        {
-            if (__instance.gameObject.scene.name == "Main Menu" || __instance.gameObject.scene.name == "Interface")
-            {
-                return false;
-            }
-
-            string text;
-            if (string.IsNullOrEmpty(__instance.file))
-            {
-                text = SaveManager.inst.CurrentStoryLevel.BeatmapJson.text;
-
-                CoreHelper.UpdateDiscordStatus("Level: " + SaveManager.inst.CurrentStoryLevel.SongName, "Playing Story", "arcade");
-            }
-            else
-            {
-                if (__instance.isYAML)
-                {
-                    string text2 = (Resources.Load("terminal/" + __instance.location + "/" + __instance.file) as TextAsset).text;
-                    object graph = new DeserializerBuilder().Build().Deserialize(new StringReader(text2));
-                    text = new SerializerBuilder().JsonCompatible().Build().Serialize(graph);
-                    LSText.CopyToClipboard(text);
-                }
-                else
-                    text = (Resources.Load("terminal/" + __instance.location + "/" + __instance.file) as TextAsset).text;
-            }
-
-            __instance.terminal.GetComponent<InterfaceController>().ParseLilScript(text);
-
-            InputDataManager.inst.playersCanJoin = __instance.playersCanJoin;
-            return false;
-        }
+        static bool StartPrefix() => false;
     }
 }
