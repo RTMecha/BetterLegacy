404b8b40f31c1fe8772532dd2e75c81724351c34
RTMecha <125487712+RTMecha@users.noreply.github.com>
2025-05-02 00:15:52 +1000

    Refractored ObjectAnimationResult to ObjectTransform

---
+++ Core/Data/Beatmap/ObjectTransform.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+namespace BetterLegacy.Core.Data.Beatmap
+{
+    public struct ObjectTransform
+    {
+        public static ObjectTransform Default => new ObjectTransform(Vector3.zero, Vector2.one, 0f);
+
+        public ObjectTransform(Vector3 position, Vector2 scale, float rotation)
+        {
+            this.position = position;
+            this.scale = scale;
+            this.rotation = rotation;
+        }
+
+        public Vector3 position;
+        public Vector2 scale;
+        public float rotation;
+    }
+}
