be5b952ff1a2f7ef565538d025e9ea805af9579b
RTMecha <125487712+RTMecha@users.noreply.github.com>
2025-05-08 21:04:30 +1000

    Store hidden in object editor data

--- Core/Data/Beatmap/ObjectEditorData.cs
+++ Core/Data/Beatmap/ObjectEditorData.cs
@@ -18,6 +18,16 @@ namespace BetterLegacy.Core.Data.Beatmap
             this.collapse = collapse;
             this.locked = locked;
         }
+        
+        public ObjectEditorData(int bin, int layer, bool collapse, bool locked, bool selectable, bool hidden)
+        {
+            Bin = bin;
+            Layer = layer;
+            this.collapse = collapse;
+            this.locked = locked;
+            this.selectable = selectable;
+            this.hidden = hidden;
+        }
 
         int bin;
         /// <summary>
@@ -50,17 +60,27 @@ namespace BetterLegacy.Core.Data.Beatmap
         public bool locked;
 
         /// <summary>
+        /// If the object is highlightable and selectable in the editor preview window.
+        /// </summary>
+        public bool selectable = true;
+
+        /// <summary>
+        /// If the object is hidden in the editor.
+        /// </summary>
+        public bool hidden = false;
+
+        /// <summary>
         /// If the editor data should serialize to JSON.
         /// </summary>
-        public bool ShouldSerialize => Bin != 0 || Layer != 0 || collapse || locked;
+        public bool ShouldSerialize => Bin != 0 || Layer != 0 || collapse || locked || !selectable || hidden;
 
         #region Methods
 
-        public static ObjectEditorData DeepCopy(ObjectEditorData orig) => new ObjectEditorData(orig.Bin, orig.Layer, orig.collapse, orig.locked);
+        public static ObjectEditorData DeepCopy(ObjectEditorData orig) => new ObjectEditorData(orig.Bin, orig.Layer, orig.collapse, orig.locked, orig.selectable, orig.hidden);
 
         public static ObjectEditorData ParseVG(JSONNode jn) => new ObjectEditorData(jn["b"].AsInt, Mathf.Clamp(jn["l"].AsInt, 0, int.MaxValue), jn["lk"].AsBool, jn["co"].AsBool);
 
-        public static ObjectEditorData Parse(JSONNode jn) => new ObjectEditorData(jn["bin"].AsInt, Mathf.Clamp(jn["layer"].AsInt, 0, int.MaxValue), jn["shrink"] == null ? jn["collapse"].AsBool : jn["shrink"].AsBool, jn["locked"].AsBool);
+        public static ObjectEditorData Parse(JSONNode jn) => new ObjectEditorData(jn["bin"].AsInt, Mathf.Clamp(jn["layer"].AsInt, 0, int.MaxValue), jn["shrink"] == null ? jn["collapse"].AsBool : jn["shrink"].AsBool, jn["locked"].AsBool, jn["select"] != null ? jn["select"].AsBool : true, jn["hide"].AsBool);
 
         public JSONNode ToJSONVG()
         {
@@ -86,6 +106,10 @@ namespace BetterLegacy.Core.Data.Beatmap
                 jn["collapse"] = collapse;
             if (locked)
                 jn["locked"] = locked;
+            if (!selectable)
+                jn["select"] = selectable;
+            if (hidden)
+                jn["hide"] = hidden;
 
             return jn;
         }
