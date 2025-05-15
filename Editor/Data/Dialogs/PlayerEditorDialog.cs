bcd744aa9d4af78ad15e25ef74188885899b74ed
RTMecha <125487712+RTMecha@users.noreply.github.com>
2025-05-15 17:40:31 +1000

    Plan on rework Player Editor Dialog

---
+++ Editor/Data/Dialogs/PlayerEditorDialog.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BetterLegacy.Editor.Data.Dialogs
+{
+    public class PlayerEditorDialog : EditorDialog
+    {
+        public PlayerEditorDialog() : base(PLAYER_EDITOR) { }
+
+        public override void Init()
+        {
+            if (init)
+                return;
+
+            base.Init();
+        }
+    }
+}
