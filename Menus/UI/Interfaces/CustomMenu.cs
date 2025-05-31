be88a13bd1b5251735762b0da9b2e617e85274a3
RTMecha <125487712+RTMecha@users.noreply.github.com>
2025-05-31 10:26:21 +1000

    Added PA Chat menu as a default interface that can be parsed

--- Menus/UI/Interfaces/CustomMenu.cs
+++ Menus/UI/Interfaces/CustomMenu.cs
@@ -56,6 +56,344 @@ namespace BetterLegacy.Menus.UI.Interfaces
             customMenu.name = jn["name"];
             customMenu.musicName = string.IsNullOrEmpty(jn["music_name"]) ? InterfaceManager.RANDOM_MUSIC_NAME : jn["music_name"];
             customMenu.allowCustomMusic = jn["allow_custom_music"] != null ? jn["allow_custom_music"].AsBool : true;
+
+            if (jn["type"] != null)
+            {
+                switch (jn["type"].Value.ToLower())
+                {
+                    case "chat": {
+                            var returnInterface = jn["return_interface"];
+                            var returnInterfacePath = jn["return_interface_path"];
+                            var seen = jn["seen"];
+
+                            System.Func<JSONArray> onScrollUpFuncJSON = () => new JSONArray
+                            {
+                                [0] = new JSONObject
+                                {
+                                    ["name"] = "ScrollLayout",
+                                    ["params"] = new JSONArray
+                                    {
+                                        [0] = "chat_layout",
+                                        [1] = "-68",
+                                        [2] = "True",
+                                    },
+                                },
+                                [1] = new JSONObject
+                                {
+                                    ["if_func"] = new JSONObject
+                                    {
+                                        ["name"] = "LayoutScrollYGreater",
+                                        ["params"] = new JSONArray { [0] = "chat_layout", [1] = "0" }
+                                    }
+                                        ["name"] = "PlaySound",
+                                    ["params"] = new JSONArray { [0] = "Click" }
+                                },
+                            };
+                            System.Func<JSONArray> onScrollDownFuncJSON = () => new JSONArray
+                            {
+                                [0] = new JSONObject
+                                {
+                                    ["name"] = "ScrollLayout",
+                                    ["params"] = new JSONArray
+                                    {
+                                        [0] = "chat_layout",
+                                        [1] = "68",
+                                        [2] = "True",
+                                    },
+                                },
+                                [1] = new JSONObject
+                                {
+                                    ["name"] = "PlaySound",
+                                    ["params"] = new JSONArray { [0] = "Click" }
+                                },
+                            };
+
+                            customMenu.layouts["buttons"] = new MenuVerticalLayout
+                            {
+                                childControlWidth = true,
+                                childForceExpandWidth = true,
+                                spacing = 4f,
+                                rect = RectValues.Default.AnchoredPosition(-500f, 250f).SizeDelta(800f, 200f),
+                            };
+                            customMenu.layouts["chat_layout"] = new MenuVerticalLayout
+                            {
+                                childControlWidth = true,
+                                childForceExpandWidth = true,
+                                spacing = 4f,
+                                rect = RectValues.FullAnchored.AnchoredPosition(0f, -40f).SizeDelta(-256f, -470f),
+                                scrollable = true,
+                                minScroll = 0f,
+                                maxScroll = 10000f,
+                                mask = true,
+                                onScrollUpFuncJSON = onScrollUpFuncJSON(),
+                                onScrollDownFuncJSON = onScrollDownFuncJSON(),
+                            };
+
+                            customMenu.elements.Add(new MenuEvent
+                            {
+                                id = "632626",
+                                name = "Effect",
+                                funcJSON = new JSONObject { ["name"] = "SetDefaultEvents" },
+                                length = 0f,
+                                wait = false,
+                            });
+                            var dateTime = System.DateTime.Now;
+                            customMenu.elements.AddRange(GenerateTopBar($"PA Chat, stable {dateTime.ToString("yyyy")}.{dateTime.ToString("MM")}.{Story.StoryManager.inst.CurrentSave.Slot}", 6, 0f));
+                            customMenu.elements.Add(new MenuImage
+                            {
+                                id = "6267583525",
+                                name = "Bar",
+                                color = 6,
+                                opacity = 0.2f,
+                                rect = RectValues.Default.AnchoredPosition(870f, -38f).SizeDelta(64f, 608f),
+                                length = 0f,
+                            });
+                            customMenu.elements.AddRange(GenerateBottomBar(6, 0f));
+
+                            var forJN = Parser.NewJSONObject();
+                            forJN["type"] = "For";
+                            forJN["from"] = "JSON";
+                            forJN["element_prefabs"][0] = new JSONObject
+                            {
+                                ["type"] = "Image",
+                                ["id"] = "1",
+                                ["name"] = "Parent",
+                                ["parent_layout"] = "chat_layout",
+                                ["rect"] = new JSONObject
+                                {
+                                    ["size"] = new JSONArray { [0] = "1400", [1] = "64" }
+                                },
+                                ["text_rect"] = new JSONObject
+                                {
+                                    ["size"] = new JSONArray { [0] = "-64", [1] = "0" }
+                                },
+                                ["col"] = "6",
+                                ["opacity"] = "0.05",
+                                ["anim_length"] = "0",
+                                ["on_scroll_up_func"] = onScrollUpFuncJSON(),
+                                ["on_scroll_down_func"] = onScrollDownFuncJSON(),
+                            };
+                            forJN["element_prefabs"][1] = new JSONObject
+                            {
+                                ["type"] = "Text",
+                                ["id"] = "2",
+                                ["name"] = "Name",
+                                ["rect"] = new JSONObject
+                                {
+                                    ["anc_max"] = new JSONArray { [0] = "1", [1] = "0.5" },
+                                    ["anc_min"] = new JSONArray { [0] = "1", [1] = "0.5" },
+                                    ["size"] = new JSONArray { [0] = "1100", [1] = "64" },
+                                },
+                                ["text_rect"] = new JSONObject
+                                {
+                                    ["size"] = new JSONArray { [0] = "-64", [1] = "0" },
+                                },
+                                ["col"] = "6",
+                                ["opacity"] = "0.05",
+                                ["text_col"] = "6",
+                                ["anim_length"] = "0",
+                                ["on_scroll_up_func"] = onScrollUpFuncJSON(),
+                                ["on_scroll_down_func"] = onScrollDownFuncJSON(),
+                            };
+                            forJN["element_prefabs"][2] = new JSONObject
+                            {
+                                ["type"] = "Text",
+                                ["id"] = "3",
+                                ["name"] = "Date",
+                                ["rect"] = new JSONObject
+                                {
+                                    ["anc_max"] = new JSONArray { [0] = "1", [1] = "0.5" },
+                                    ["anc_min"] = new JSONArray { [0] = "1", [1] = "0.5" },
+                                    ["size"] = new JSONArray { [0] = "0", [1] = "64" },
+                                },
+                                ["text_rect"] = new JSONObject
+                                {
+                                    ["size"] = new JSONArray { [0] = "-64", [1] = "0" },
+                                },
+                                ["hide_bg"] = "True",
+                                ["text_col"] = "6",
+                                ["anim_length"] = "0",
+                                ["on_scroll_up_func"] = onScrollUpFuncJSON(),
+                                ["on_scroll_down_func"] = onScrollDownFuncJSON(),
+                            };
+                            forJN["element_prefabs"][3] = new JSONObject
+                            {
+                                ["type"] = "Text",
+                                ["id"] = "4",
+                                ["name"] = "Chat",
+                                ["rect"] = "FullAnchored",
+                                ["text_rect"] = new JSONObject
+                                {
+                                    ["size"] = new JSONArray { [0] = "-64", [1] = "0" },
+                                },
+                                ["hide_bg"] = "True",
+                                ["text_col"] = "6",
+                                ["anim_length"] = "4",
+                                ["text_sound_repeat"] = "2",
+                                ["on_scroll_up_func"] = onScrollUpFuncJSON(),
+                                ["on_scroll_down_func"] = onScrollDownFuncJSON(),
+                            };
+                            forJN["element_prefabs"][4] = new JSONObject
+                            {
+                                ["type"] = "Event",
+                                ["id"] = "5",
+                                ["name"] = "Wait",
+                                ["anim_length"] = "1",
+                            };
+
+                            for (int i = 0; i < jn["dialogue"].Count; i++)
+                            {
+                                var id = LSText.randomNumString(16);
+                                var jnDialogue = jn["dialogue"][i];
+                                string character = jnDialogue["character"];
+                                string text = jnDialogue["text"];
+                                string color = jnDialogue["color"];
+                                string sound = jnDialogue["sound"];
+
+                                forJN["to"][i]["1"] = new JSONObject { ["id"] = id, };
+                                forJN["to"][i]["2"] = new JSONObject
+                                {
+                                    ["parent"] = id,
+                                    ["text"] = $"| {character}",
+                                };
+                                forJN["to"][i]["3"] = new JSONObject
+                                {
+                                    ["parent"] = id,
+                                    ["text"] = "<align=right>{{Date=HH:mm:ss:tt}} |",
+                                };
+                                var jnText = new JSONObject
+                                {
+                                    ["parent"] = id,
+                                    ["text"] = text,
+                                    ["anim_length"] = jnDialogue["length"],
+                                };
+                                if (!string.IsNullOrEmpty(color))
+                                    jnText["override_text_col"] = color;
+                                if (!string.IsNullOrEmpty(sound))
+                                    jnText["text_sound"] = sound;
+
+                                if (jnDialogue["speeds"] != null)
+                                {
+                                    for (int j = 0; j < jnDialogue["speeds"].Count; j++)
+                                    {
+                                        jnText["speeds"][j]["position"] = jnDialogue["speeds"][j]["position"];
+                                        jnText["speeds"][j]["speed"] = jnDialogue["speeds"][j]["speed"];
+                                    }
+                                }
+
+                                if (jnDialogue["text_sound_volume"] != null)
+                                    jnText["text_sound_volume"] = jnDialogue["text_sound_volume"];
+                                if (jnDialogue["text_sound_pitch"] != null)
+                                    jnText["text_sound_pitch"] = jnDialogue["text_sound_pitch"];
+                                if (jnDialogue["text_sound_pitch_vary"] != null)
+                                    jnText["text_sound_pitch_vary"] = jnDialogue["text_sound_pitch_vary"];
+                                if (jnDialogue["text_sound_repeat"] != null)
+                                    jnText["text_sound_repeat"] = jnDialogue["text_sound_repeat"];
+
+                                forJN["to"][i]["4"] = jnText;
+
+                                forJN["to"][i]["5"] = new JSONObject { ["anim_length"] = jnDialogue["wait"], };
+                            }
+
+                            var elements = Parser.NewJSONArray();
+                            elements[0] = forJN;
+                            customMenu.elements.AddRange(ParseElements(elements));
+
+                            customMenu.elements.Add(new MenuImage
+                            {
+                                id = "6267583525",
+                                name = "Bar",
+                                color = 6,
+                                opacity = 0.2f,
+                                rect = RectValues.Default.AnchoredPosition(870f, -38f).SizeDelta(64f, 608f),
+                                onScrollUpFuncJSON = onScrollUpFuncJSON(),
+                                onScrollDownFuncJSON = onScrollDownFuncJSON(),
+                            });
+                            customMenu.elements.Add(new MenuButton
+                            {
+                                id = "3627564652",
+                                name = "Up Button",
+                                selectionPosition = new Vector2Int(1, 0),
+                                iconPath = RTFile.GetAsset("editor_gui_up.png"),
+                                color = 6,
+                                rect = RectValues.Default.AnchoredPosition(870f, 234f).SizeDelta(64f, 64f),
+                                iconRect = RectValues.Default.SizeDelta(64f, 64f),
+                                funcJSON = onScrollUpFuncJSON(),
+                                onScrollUpFuncJSON = onScrollUpFuncJSON(),
+                                onScrollDownFuncJSON = onScrollDownFuncJSON(),
+                            });
+                            customMenu.elements.Add(new MenuButton
+                            {
+                                id = "95578583",
+                                name = "Down Button",
+                                selectionPosition = new Vector2Int(1, 1),
+                                iconPath = RTFile.GetAsset("editor_gui_down.png"),
+                                color = 6,
+                                rect = RectValues.Default.AnchoredPosition(870f, -310f).SizeDelta(64f, 64f),
+                                iconRect = RectValues.Default.SizeDelta(64f, 64f),
+                                funcJSON = onScrollDownFuncJSON(),
+                                onScrollUpFuncJSON = onScrollUpFuncJSON(),
+                                onScrollDownFuncJSON = onScrollDownFuncJSON(),
+                            });
+                            customMenu.elements.Add(new MenuButton
+                            {
+                                id = "15215",
+                                name = "Back Button",
+                                parentLayout = "buttons",
+                                text = "<b> [ RETURN ]",
+                                selectionPosition = Vector2Int.zero,
+                                rect = RectValues.Default.SizeDelta(200f, 64f),
+                                color = 6,
+                                selectedColor = 6,
+                                opacity = 0.1f,
+                                selectedOpacity = 1f,
+                                textColor = 6,
+                                selectedTextColor = 7,
+                                length = 1f,
+                                playBlipSound = true,
+                                funcJSON = new JSONArray
+                                {
+                                    [0] = new JSONObject { ["name"] = "ClearInterfaces" },
+                                    [1] = new JSONObject
+                                    {
+                                        ["name"] = "Parse",
+                                        ["params"] = new JSONObject
+                                        {
+                                            ["file"] = returnInterface,
+                                            ["load"] = "True",
+                                            ["path"] = returnInterfacePath,
+                                        },
+                                    },
+                                    [2] = new JSONObject
+                                    {
+                                        ["name"] = "StorySaveBool",
+                                        ["params"] = new JSONArray
+                                        {
+                                            [0] = seen,
+                                            [1] = "True",
+                                        },
+                                    }
+                                },
+                            });
+
+                            customMenu.elements.Add(new MenuImage
+                            {
+                                id = "374848",
+                                name = "Background",
+                                siblingIndex = 0,
+                                rect = RectValues.FullAnchored,
+                                opacity = 0f,
+                                length = 0f,
+                                wait = false,
+                                onScrollUpFuncJSON = onScrollUpFuncJSON(),
+                                onScrollDownFuncJSON = onScrollDownFuncJSON(),
+                            });
+
+                            return customMenu;
+                        }
+                }
+            }
+
             customMenu.defaultSelection = Parser.TryParse(jn["default_select"], Vector2Int.zero);
             customMenu.forceInterfaceSpeed = jn["force_speed"].AsBool;
             if (jn["layer"] != null)
