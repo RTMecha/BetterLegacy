1d35e01856c79ec9d93187b7acd666b3568fa0b1
RTMecha <125487712+RTMecha@users.noreply.github.com>
2025-06-18 23:13:16 +1000

    Version 1.7.3

--- LegacyPlugin.cs
+++ LegacyPlugin.cs
@@ -28,7 +28,7 @@ namespace BetterLegacy
     /// <summary>
     /// Core plugin class.
     /// </summary>
-    [BepInPlugin("com.mecha.betterlegacy", "Better Legacy", "1.7.2")]
+    [BepInPlugin("com.mecha.betterlegacy", "Better Legacy", "1.7.3")]
     [BepInProcess("Project Arrhythmia.exe")]
     public class LegacyPlugin : BaseUnityPlugin
     {
