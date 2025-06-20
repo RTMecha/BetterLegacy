2b7806545a2a87945d79656194f03fd016d7cfac
RTMecha <125487712+RTMecha@users.noreply.github.com>
2025-06-21 09:57:38 +1000

    GetPrefabables

--- Core/Data/Beatmap/GameData.cs
+++ Core/Data/Beatmap/GameData.cs
@@ -1761,6 +1761,14 @@ namespace BetterLegacy.Core.Data.Beatmap
             }
         }
 
+        public List<IPrefabable> GetPrefabables()
+        {
+            var prefabables = beatmapObjects.Select(x => x as IPrefabable).ToList();
+            prefabables.AddRange(backgroundObjects);
+            prefabables.AddRange(backgroundLayers);
+            return prefabables;
+        }
+
         public List<BeatmapTheme> GetUsedThemes() => GetUsedThemes(beatmapThemes);
 
         public List<BeatmapTheme> GetUsedThemes(List<BeatmapTheme> beatmapThemes) => events == null || events.Count <= 4 ? new List<BeatmapTheme>() : beatmapThemes.Where(x => Parser.TryParse(x.id, 0) != 0 && events[4].Has(y => y.values[0] == Parser.TryParse(x.id, 0))).ToList();
