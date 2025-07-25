29be3bf461761ec1497f5a8b84344cf000bd6a90
RTMecha <125487712+RTMecha@users.noreply.github.com>
2025-07-25 13:34:16 +1000

    Reactive interface

---
+++ Core/Data/Beatmap/IReactive.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+namespace BetterLegacy.Core.Data.Beatmap
+{
+    /// <summary>
+    /// Indicates an object can react to the audio.
+    /// </summary>
+    public interface IReactive
+    {
+        /// <summary>
+        /// Position reacts to the audio.
+        /// </summary>
+        public Vector3 ReactivePositionOffset { get; set; }
+        /// <summary>
+        /// Scale reacts to the audio.
+        /// </summary>
+        public Vector3 ReactiveScaleOffset { get; set; }
+        /// <summary>
+        /// Rotation reacts to the audio.
+        /// </summary>
+        public float ReactiveRotationOffset { get; set; }
+    }
+}
