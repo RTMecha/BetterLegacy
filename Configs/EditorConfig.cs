5ed6fd1a89b6c8ae591841599836c57befe518a7
RTMecha <125487712+RTMecha@users.noreply.github.com>
2025-08-03 15:29:54 +1000

    Display achievements setting

--- Configs/EditorConfig.cs
+++ Configs/EditorConfig.cs
@@ -1496,6 +1496,11 @@ namespace BetterLegacy.Configs
         /// </summary>
         public Setting<bool> ModifiersSavesBackup { get; set; }
 
+        /// <summary>
+        /// If the achievement modifiers display the achievement they unlock in the editor regardless of unlock state.
+        /// </summary>
+        public Setting<bool> ModifiersDisplayAchievements { get; set; }
+
         #endregion
 
         #endregion
@@ -2940,6 +2945,7 @@ namespace BetterLegacy.Configs
 
             ModifiersCanLoadLevels = Bind(this, MODIFIERS, "Modifiers Can Load Levels", true, "Any modifiers with the \"loadLevel\" function will load the level whilst in the editor. This is only to prevent the loss of progress.");
             ModifiersSavesBackup = Bind(this, MODIFIERS, "Modifiers Saves Backup", true, "The current level will have a backup saved before a level is loaded using a loadLevel modifier or before the game has been quit.");
+            ModifiersDisplayAchievements = Bind(this, MODIFIERS, "Modifiers Display Achievements", true, "If the achievement modifiers display the achievement they unlock in the editor regardless of unlock state.");
 
             #endregion
 
