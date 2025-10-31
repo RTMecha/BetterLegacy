61763c5a8745f7c5cb83bd4af6e5776e8dde80c6
RTMecha <125487712+RTMecha@users.noreply.github.com>
2025-11-01 10:32:11 +1100

    Added summaries and changed namespace to match folder

--- Editor/Data/IEditorLayerUI.cs
+++ Editor/Data/IEditorLayerUI.cs
@@ -1,15 +1,24 @@
 ﻿using UnityEngine;
 using UnityEngine.UI;
 
-namespace BetterLegacy.Editor.Data.Dialogs
+namespace BetterLegacy.Editor.Data
 {
     /// <summary>
     /// Indicates a UI has a layer value that can be edited via an input field or a toggle list.
     /// </summary>
     public interface IEditorLayerUI
     {
+        /// <summary>
+        /// Input field for practically unlimited editor layers.
+        /// </summary>
         public InputField EditorLayerField { get; set; }
+        /// <summary>
+        /// Toggles parent.
+        /// </summary>
         public RectTransform EditorLayerTogglesParent { get; set; }
+        /// <summary>
+        /// Array of editor layer toggles.
+        /// </summary>
         public Toggle[] EditorLayerToggles { get; set; }
     }
 }
