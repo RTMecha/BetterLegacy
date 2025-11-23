2742999b64a5ca5206915f280e93b88648e53b2c
RTMecha <125487712+RTMecha@users.noreply.github.com>
2025-11-23 13:03:51 +1100

    Summary and sort code

--- Core/Data/Player/PAPlayer.cs
+++ Core/Data/Player/PAPlayer.cs
@@ -15,6 +15,9 @@ using BetterLegacy.Menus.UI.Interfaces;
 
 namespace BetterLegacy.Core.Data.Player
 {
+    /// <summary>
+    /// Represents a player in a Project Arrhythmia level.
+    /// </summary>
     public class PAPlayer : Exists, IModifierReference, ICustomActivatable
     {
         public PAPlayer(bool active, int index, InputDevice device)
@@ -29,7 +32,7 @@ namespace BetterLegacy.Core.Data.Player
             {
                 deviceName = device.Name;
                 controllerConnected = true;
-                SortOrder = device.SortOrder;
+                sortOrder = device.SortOrder;
             }
             else
                 deviceName = "keyboard";
@@ -54,65 +57,43 @@ namespace BetterLegacy.Core.Data.Player
 
         #region Values
 
+        /// <summary>
+        /// The main player.
+        /// </summary>
         public static PAPlayer Main => PlayerManager.Players[0];
 
+        /// <summary>
+        /// Identification string.
+        /// </summary>
         public string id = PAObjectBase.GetStringID();
 
+        /// <summary>
+        /// If the player has been spawned.
+        /// </summary>
         public bool active;
 
+        /// <summary>
+        /// The current lives count of the player.
+        /// </summary>
         public int lives = -1;
         /// <summary>
         /// If the player is out of lives.
         /// </summary>
         public bool OutOfLives => lives == 0;
 
+        /// <summary>
+        /// Health the player has.
+        /// </summary>
         public int health = 3;
 
-        public int index;
-
-        public string deviceName;
-
-        public ControllerType deviceType;
-
-        public string deviceModel;
-
-        public InputDevice device;
-
-        public int SortOrder;
-
-        public bool controllerConnected;
-
-        public PlayerIndex playerIndex;
-
-        public Vector2 rumble;
-
         /// <summary>
-        /// If the player is currently alive.
+        /// The maximum amount of health the players have.
         /// </summary>
-        public bool Alive => RuntimePlayer;
-
-        public bool CustomActive { get; set; } = true;
-
-        public RTPlayer RuntimePlayer { get; set; }
-
-
-        public string currentPlayerModel = PlayerModel.DEFAULT_ID;
-        public string CurrentModel
-        {
-            get => !CoreHelper.InEditor && PlayersData.AllowCustomModels && PlayerConfig.Instance.LoadFromGlobalPlayersInArcade.Value ? PlayerManager.PlayerIndexes[index].Value : currentPlayerModel;
-            set
-            {
-                currentPlayerModel = value;
-                UpdatePlayerModel();
-            }
-        }
-
-        public PlayerModel PlayerModel { get; set; } = PlayerModel.DefaultPlayer;
-
-        public PlayerInventory inventory = new PlayerInventory();
-
         public static int MaxHealth { get; set; } = 3;
 
+        /// <summary>
+        /// Health the player has. If it reaches 0, the player dies.
+        /// </summary>
         public int Health
         {
             get => Mathf.Clamp(health, 0, MaxHealth);
@@ -124,6 +105,91 @@ namespace BetterLegacy.Core.Data.Player
             }
         }
 
+        /// <summary>
+        /// Index of the player in <see cref="PlayerManager.Players"/>.
+        /// </summary>
+        public int index;
+
+        /// <summary>
+        /// Controller device name.
+        /// </summary>
+        public string deviceName;
+
+        /// <summary>
+        /// Controller device type.
+        /// </summary>
+        public ControllerType deviceType;
+
+        /// <summary>
+        /// Controller device model.
+        /// </summary>
+        public string deviceModel;
+
+        /// <summary>
+        /// Player controller reference.
+        /// </summary>
+        public InputDevice device;
+
+        /// <summary>
+        /// <see cref="InputDevice.SortOrder"/> cache.
+        /// </summary>
+        public int sortOrder;
+
+        /// <summary>
+        /// If the players' controller is connected.
+        /// </summary>
+        public bool controllerConnected;
+
+        /// <summary>
+        /// Index of the player according to <see cref="PlayerIndex"/>.
+        /// </summary>
+        public PlayerIndex playerIndex;
+
+        /// <summary>
+        /// Amount the controller is rumbling.
+        /// </summary>
+        public Vector2 rumble;
+
+        /// <summary>
+        /// If the player is currently alive.
+        /// </summary>
+        public bool Alive => RuntimePlayer;
+
+        /// <summary>
+        /// The custom active state.
+        /// </summary>
+        public bool CustomActive { get; set; } = true;
+
+        /// <summary>
+        /// The runtime player reference.
+        /// </summary>
+        public RTPlayer RuntimePlayer { get; set; }
+
+        /// <summary>
+        /// The current player model ID.
+        /// </summary>
+        public string currentPlayerModel = PlayerModel.DEFAULT_ID;
+        /// <summary>
+        /// The current player model ID.
+        /// </summary>
+        public string CurrentModel
+        {
+            get => !CoreHelper.InEditor && PlayersData.AllowCustomModels && PlayerConfig.Instance.LoadFromGlobalPlayersInArcade.Value ? PlayerManager.PlayerIndexes[index].Value : currentPlayerModel;
+            set
+            {
+                currentPlayerModel = value;
+                UpdatePlayerModel();
+            }
+        }
+
+        /// <summary>
+        /// The current player model cache.
+        /// </summary>
+        public PlayerModel PlayerModel { get; set; } = PlayerModel.DefaultPlayer;
+
+        // might not go with this?
+        public PlayerInventory inventory = new PlayerInventory();
+
         public RTLevelBase ParentRuntime { get; set; }
 
         public ModifierReferenceType ReferenceType => ModifierReferenceType.PAPlayer;
@@ -132,7 +198,7 @@ namespace BetterLegacy.Core.Data.Player
 
         #endregion
 
-        #region Methods
+        #region Functions
 
         public void SetCustomActive(bool active)
         {
@@ -140,6 +206,9 @@ namespace BetterLegacy.Core.Data.Player
             RuntimePlayer?.SetActive(active);
         }
 
+        /// <summary>
+        /// Updates player model data.
+        /// </summary>
         public void UpdatePlayerModel()
         {
             PlayerModel = PlayersData.GetPlayerModel(CurrentModel);
@@ -147,16 +216,42 @@ namespace BetterLegacy.Core.Data.Player
                 RuntimePlayer.Model = PlayerModel;
         }
 
+        /// <summary>
+        /// Gets the players' maximum amount of health.
+        /// </summary>
+        /// <returns>Returns the challenge mode default health if the challenge mode default health is greater than 0, otherwise returns the players' local health.</returns>
         public int GetMaxHealth() => RTBeatmap.Current.challengeMode.DefaultHealth > 0 ? RTBeatmap.Current.challengeMode.DefaultHealth : GetControl()?.Health ?? 3;
 
+        /// <summary>
+        /// Gets the players' maximum amount of lives.
+        /// </summary>
+        /// <returns>Returns the challenge mode lives count if the challenge mode lives count is greater than 0, otherwise returns the player controls' lives count.</returns>
         public int GetMaxLives() => RTBeatmap.Current.challengeMode.Lives > 0 ? RTBeatmap.Current.challengeMode.Lives : GetControl()?.lives ?? -1;
 
+        /// <summary>
+        /// Gets the players' control data.
+        /// </summary>
+        /// <returns>Returns the player model converted to a player control if <see cref="LevelData.allowPlayerModelControls"/> is true, otherwise returns <see cref="GetCustomControl"/>.</returns>
         public PlayerControl GetControl() => GameData.Current.data.level.allowPlayerModelControls ? PlayerModel.ToPlayerControl() : GetCustomControl();
 
+        /// <summary>
+        /// Gets the player control data.
+        /// </summary>
+        /// <returns>Returns the player control data associated with this player.</returns>
         public PlayerControl GetCustomControl() => PlayersData.Current && PlayersData.Current.playerControls.TryGetAt(index, out PlayerControl playerControl) ? playerControl : new PlayerControl();
 
-        public PlayerIndex GetPlayerIndex(int _index) => (PlayerIndex)_index;
+        /// <summary>
+        /// Gets the <see cref="PlayerIndex"/> value for the player.
+        /// </summary>
+        /// <param name="index">Index of the player controller.</param>
+        /// <returns>Returns the <paramref name="index"/> casted to <see cref="PlayerIndex"/>. This is done so more than 4 players is possible.</returns>
+        public PlayerIndex GetPlayerIndex(int index) => (PlayerIndex)index;
 
+        /// <summary>
+        /// Gets the <see cref="ControllerType"/> of the players' controller.
+        /// </summary>
+        /// <param name="deviceName">Device name.</param>
+        /// <returns>Returns the controller type of the device.</returns>
         public ControllerType GetDeviceType(string deviceName)
         {
             deviceName = deviceName.ToLower().Remove(" ");
@@ -169,6 +264,11 @@ namespace BetterLegacy.Core.Data.Player
             return ControllerType.Unknown;
         }
 
+        /// <summary>
+        /// Gets the device model of the players' controller.
+        /// </summary>
+        /// <param name="deviceName">Device name.</param>
+        /// <returns>Returns the device model name of the device.</returns>
         public string GetDeviceModel(string deviceName) => GetDeviceType(deviceName) switch
         {
             ControllerType.XBox => "XBox Controller",
@@ -177,9 +277,13 @@ namespace BetterLegacy.Core.Data.Player
             _ => "Unknown Controller",
         };
 
+        /// <summary>
+        /// Function for <see cref="InputManager.OnDeviceDetached"/>
+        /// </summary>
+        /// <param name="device">The device that was disconnected.</param>
         public void ControllerDisconnected(InputDevice device)
         {
-            if (device.SortOrder != SortOrder || GetDeviceModel(device.Name) != deviceModel)
+            if (device.SortOrder != sortOrder || GetDeviceModel(device.Name) != deviceModel)
                 return;
 
             if (InputSelectMenu.Current)
@@ -204,9 +308,13 @@ namespace BetterLegacy.Core.Data.Player
             Debug.LogFormat("{0}Disconnected Controler was attached to player. Controller [{1}] [{2}] -/- Player [{3}]", InputDataManager.className, device.Name, device.SortOrder, index);
         }
 
+        /// <summary>
+        /// Function for <see cref="InputManager.OnDeviceAttached"/>
+        /// </summary>
+        /// <param name="device">The device that was connected.</param>
         public void ControllerConnected(InputDevice device)
         {
-            if (device.SortOrder != SortOrder || GetDeviceModel(device.Name) != deviceModel)
+            if (device.SortOrder != sortOrder || GetDeviceModel(device.Name) != deviceModel)
                 return;
 
             InputDataManager.inst.ThereIsNoPlayerUsingJoystick(device);
@@ -222,7 +330,16 @@ namespace BetterLegacy.Core.Data.Player
             Debug.LogFormat("{0}Connected Controller exists in players. Controller [{1}] [{2}] -> Player [{3}]", InputDataManager.className, device.Name, device.SortOrder, index);
         }
 
-        public void ResetHealth() => Health = GetControl()?.Health ?? 3;
+        /// <summary>
+        /// Resets the players' health to the current default.
+        /// </summary>
+        public void ResetHealth() => Health = GetDefaultHealth();
+
+        /// <summary>
+        /// Gets the default health for this player.
+        /// </summary>
+        /// <returns>Returns the challenge mode default health if the user is not editing and the challenge mode default health is greater than 0, otherwise returns the players' local health.</returns>
+        public int GetDefaultHealth() => !CoreHelper.IsEditing && RTBeatmap.Current.challengeMode.DefaultHealth > 0 ? RTBeatmap.Current.challengeMode.DefaultHealth : GetControl()?.Health ?? 3;
 
         public IRTObject GetRuntimeObject() => RuntimePlayer;
 
