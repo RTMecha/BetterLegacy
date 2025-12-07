f532355f22ee040a032c9e7af395bcacc29feb3c
RTMecha <125487712+RTMecha@users.noreply.github.com>
2025-12-08 08:36:23 +1100

    Look into reworking context menu elements

--- Editor/Data/ButtonFunction.cs
+++ Editor/Data/ButtonFunction.cs
@@ -1,9 +1,89 @@
 ﻿using System;
 
+using UnityEngine;
 using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+using BetterLegacy.Core;
+using BetterLegacy.Core.Data;
+using BetterLegacy.Core.Helpers;
+using BetterLegacy.Core.Prefabs;
+using BetterLegacy.Editor.Managers;
 
 namespace BetterLegacy.Editor.Data
 {
+    public abstract class EditorElement
+    {
+        public RectValues rect;
+        public string tooltipGroup;
+
+        public virtual float ContextMenuHeight => 0f;
+
+        public abstract void Init(InitSettings initSettings);
+
+        public void InitTooltip(GameObject gameObject)
+        {
+            if (!string.IsNullOrEmpty(tooltipGroup))
+                TooltipHelper.AssignTooltip(gameObject, tooltipGroup);
+        }
+
+        public struct InitSettings
+        {
+            public Transform parent;
+
+            public InitSettings Parent(Transform parent)
+            {
+                this.parent = parent;
+                return this;
+            }
+        }
+    }
+
+    public class SpacerElement : EditorElement
+    {
+        public Vector2 size = new Vector2(0f, 4f);
+
+        public override float ContextMenuHeight => 6f;
+
+        public override void Init(InitSettings initSettings)
+        {
+            if (!initSettings.parent)
+                return;
+
+            var gameObject = Creator.NewUIObject("sp", initSettings.parent);
+            var image = gameObject.AddComponent<Image>();
+            image.rectTransform.sizeDelta = size;
+            EditorThemeManager.ApplyGraphic(image, ThemeGroup.Background_3);
+        }
+    }
+
+    public class ButtonElement : EditorElement
+    {
+        public string name;
+        public Action action;
+
+        public override float ContextMenuHeight => 37f;
+
+        public override void Init(InitSettings initSettings)
+        {
+            if (!initSettings.parent)
+                return;
+
+            var gameObject = EditorPrefabHolder.Instance.Function2Button.Duplicate(initSettings.parent);
+            var buttonStorage = gameObject.GetComponent<FunctionButtonStorage>();
+
+            buttonStorage.OnClick.NewListener(() => action?.Invoke());
+            buttonStorage.label.alignment = TextAnchor.MiddleLeft;
+            buttonStorage.Text = name;
+            buttonStorage.label.rectTransform.sizeDelta = new Vector2(-12f, 0f);
+
+            InitTooltip(gameObject);
+
+            EditorThemeManager.ApplySelectable(buttonStorage.button, ThemeGroup.Function_2);
+            EditorThemeManager.ApplyGraphic(buttonStorage.label, ThemeGroup.Function_2_Text);
+        }
+    }
+
     /// <summary>
     /// Handles a buttons' UI in the editor.
     /// </summary>
