a78dd54c086d9a41abc89751c4aea313ccbc7dc4
RTMecha <125487712+RTMecha@users.noreply.github.com>
2026-01-04 06:27:02 +1100

    Trying out attributes on enums

--- Core/Helpers/EnumHelper.cs
+++ Core/Helpers/EnumHelper.cs
@@ -45,5 +45,58 @@ namespace BetterLegacy.Core.Helpers
         /// <param name="value">Enum value to check.</param>
         /// <returns>Returns true if the enum value is valid, otherwise returns false.</returns>
         public static bool IsDefined<T>(T value) where T : Enum => Enum.IsDefined(typeof(T), value);
+
+        /// <summary>
+        /// Tries to get an attribute from an enum value.
+        /// </summary>
+        /// <typeparam name="TEnum">Type of the enum.</typeparam>
+        /// <typeparam name="TAttribute">Type of the attribute. Must be of type <see cref="Attribute"/>.</typeparam>
+        /// <param name="value">Enum value.</param>
+        /// <param name="attribute">Attribute result. Must be of type <see cref="Attribute"/>.</param>
+        /// <returns>Returns true if an attribute was obtained from the enum value.</returns>
+        public static bool TryGetAttribute<TEnum, TAttribute>(TEnum value, out TAttribute attribute) where TEnum : Enum where TAttribute : Attribute
+        {
+            var field = value.GetType().GetField(value.ToString());
+            if (field != null && Attribute.GetCustomAttribute(field, typeof(TAttribute)) is TAttribute a)
+            {
+                attribute = a;
+                return true;
+            }
+
+            attribute = default;
+            return false;
+        }
+
+        /// <summary>
+        /// Gets an attribute from an enum value.
+        /// </summary>
+        /// <typeparam name="TEnum">Type of the enum.</typeparam>
+        /// <typeparam name="TAttribute">Type of the attribute. Must be of type <see cref="Attribute"/>.</typeparam>
+        /// <param name="value">Enum value.</param>
+        /// <returns>Returns the found attribute.</returns>
+        public static Attribute GetAttribute<TEnum, TAttribute>(TEnum value) where TEnum : Enum => Attribute.GetCustomAttribute(value.GetType().GetField(value.ToString()), typeof(TAttribute));
+
+        public static void TestEnumAttribute()
+        {
+            if (TryGetAttribute(TestEnum.One, out TestAttribute test))
+                CoreHelper.Log($"Number is {test.number}");
+        }
+
+        public enum TestEnum
+        {
+            [Test(0)]
+            One,
+            [Test(1)]
+            Two,
+        }
+
+        public class TestAttribute : Attribute
+        {
+            public TestAttribute() { }
+
+            public TestAttribute(int number) => this.number = number;
+
+            public readonly int number;
+        }
     }
 }
