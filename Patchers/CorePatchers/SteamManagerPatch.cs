abea8d3bd64cd5f7296f34d5b7eb0affa40f294f
RTMecha <125487712+RTMecha@users.noreply.github.com>
2026-01-16 15:59:40 +1100

    Update to latest steamworks

--- Patchers/CorePatchers/SteamManagerPatch.cs
+++ Patchers/CorePatchers/SteamManagerPatch.cs
@@ -1,21 +1,39 @@
-﻿using UnityEngine;
+﻿using System;
+
+using UnityEngine;
 
 using HarmonyLib;
+using Steamworks;
 
 using BetterLegacy.Core.Managers;
 
 namespace BetterLegacy.Patchers
 {
     [HarmonyPatch(typeof(SteamManager))]
-    public class SteamManagerPatch : MonoBehaviour
+    public class SteamManagerPatch
     {
         [HarmonyPatch(nameof(SteamManager.Awake))]
-        [HarmonyPostfix]
-        static void AwakePostfix()
+        [HarmonyPrefix]
+        static bool AwakePrefix(SteamManager __instance)
         {
+            try
+            {
+                if (SteamManager.s_instance != null)
+                {
+                    UnityEngine.Object.Destroy(__instance.gameObject);
+                    return false;
+                }
+                SteamManager.s_instance = __instance;
+                UnityObject.DontDestroyOnLoad(__instance.gameObject);
+            }
+            catch
+            {
+                Debug.LogError("[Steamworks.NET] failed");
+            }
             RTSteamManager.Init();
             NetworkManager.Init();
             SteamLobbyManager.Init();
+            return false;
         }
     }
 }
