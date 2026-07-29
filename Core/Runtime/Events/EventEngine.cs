5cb78068cbdbe03fc0143916e8af70fb17d5033e
RTMecha <125487712+RTMecha@users.noreply.github.com>
2026-07-30 01:56:11 +1000

    Moved EventOffset class to its own file

--- Core/Runtime/Events/EventEngine.cs
+++ Core/Runtime/Events/EventEngine.cs
@@ -2434,42 +2434,6 @@ namespace BetterLegacy.Core.Runtime.Events
         /// </summary>
         public List<List<EventOffset>> offsets;
 
-        /// <summary>
-        /// Represents a value to offset to an event.
-        /// </summary>
-        public class EventOffset
-        {
-            #region Constructors
-
-            public EventOffset() { }
-
-            public EventOffset(float value) => this.value = value;
-
-            #endregion
-
-            #region Values
-
-            /// <summary>
-            /// Value to offset.
-            /// </summary>
-            public float value;
-
-            /// <summary>
-            /// Operation of the offset.
-            /// </summary>
-            public MathOperation operation = MathOperation.Addition;
-
-            #endregion
-
-            #region Operators
-
-            public static implicit operator float(EventOffset eventOffset) => eventOffset.value;
-
-            public static implicit operator EventOffset(float value) => new EventOffset(value);
-
-            #endregion
-        }
-
         #endregion
     }
 }
---
+++ Core/Runtime/Events/EventOffset.cs
@@ -0,0 +1,38 @@
+﻿namespace BetterLegacy.Core.Runtime.Events
+{
+    /// <summary>
+    /// Represents a value to offset to an event.
+    /// </summary>
+    public class EventOffset
+    {
+        #region Constructors
+
+        public EventOffset() { }
+
+        public EventOffset(float value) => this.value = value;
+
+        #endregion
+
+        #region Values
+
+        /// <summary>
+        /// Value to offset.
+        /// </summary>
+        public float value;
+
+        /// <summary>
+        /// Operation of the offset.
+        /// </summary>
+        public MathOperation operation = MathOperation.Addition;
+
+        #endregion
+
+        #region Operators
+
+        public static implicit operator float(EventOffset eventOffset) => eventOffset.value;
+
+        public static implicit operator EventOffset(float value) => new EventOffset(value);
+
+        #endregion
+    }
+}
